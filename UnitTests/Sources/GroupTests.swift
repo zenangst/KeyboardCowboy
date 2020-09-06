efc99f44b7558739c29686afcb7e46a91f1219b3
Christoffer Winterkvist <christoffer@winterkvist.com>
2020-09-06 16:55:42 +0200

    Refactor unit tests to use snapshot testing
    - Add SnapshotTesting as a dependency on UnitTests target
    - Refactor tests to use `assertSnapshot`

--- UnitTests/Sources/GroupTests.swift
+++ UnitTests/Sources/GroupTests.swift
@@ -1,5 +1,6 @@
-import Foundation
 @testable import LogicFramework
+import Foundation
+import SnapshotTesting
 import XCTest
 
 class GroupTests: XCTestCase {
@@ -13,9 +14,7 @@ class GroupTests: XCTestCase {
     guard let result = String(data: data, encoding: .utf8) else {
       throw GroupError.unableToProduceString
     }
-    let expected = "{\"name\":\"Global shortcuts\",\"rules\":[{\"application\":{\"name\":\"Finder\",\"bundleIdentifier\":\"com.apple.Finder\",\"path\":\"/System/Library/CoreServices/Finder.app\"}},{\"days\":[0,1,2,3,4,5,6]}],\"workflows\":[{\"commands\":[{\"applicationCommand\":{\"application\":{\"name\":\"Finder\",\"bundleIdentifier\":\"com.apple.Finder\",\"path\":\"/System/Library/CoreServices/Finder.app\"}}}],\"name\":\"Open/active Finder\"}]}"
-
-    XCTAssertEqual(result.replacingOccurrences(of: "\\", with: ""), expected)
+    assertSnapshot(matching: result, as: .dump)
   }
 
   func testJSONDecoding() throws {
