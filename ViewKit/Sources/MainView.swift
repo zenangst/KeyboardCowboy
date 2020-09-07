63cfd186fc08ed0d472df64e08b47d5cbe429fe8
Christoffer Winterkvist <christoffer@winterkvist.com>
2020-09-07 18:35:07 +0200

    Fix SwiftUI not working in ViewKit framework
    - Add `arm64` to `EXCLUDED_ARCHS`
    - Add dummy view called `MainView`

---
+++ ViewKit/Sources/MainView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
