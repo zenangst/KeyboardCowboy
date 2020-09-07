be6088c61f64bd1830a256ef9e53d645981004c9
Vadym Markov <markov.vadym@gmail.com>
2020-09-08 01:05:58 +0200

    Split view to smaller subviews

---
+++ ViewKit/Sources/Views/GroupList/GroupListCell.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+
+struct GroupListCell: View {
+  let group: ViewKit.Group
+
+  var body: some View {
+    HStack {
+      name
+      Spacer()
+      numberOfWorkflows
+    }.padding()
+  }
+}
+
+// MARK: - Subviews
+
+private extension GroupListCell {
+  var name: some View {
+    Text(group.name)
+      .foregroundColor(.primary)
+  }
+
+  var numberOfWorkflows: some View {
+    Text("\(group.workflows.count)")
+      .foregroundColor(.white)
+      .padding(.horizontal, 6)
+      .padding(.vertical, 2)
+      .background(Circle().fill(Color(.systemBlue)))
+  }
+}
+
+// MARK: - Previews
+
+struct GroupListCell_Previews: PreviewProvider {
+    static var previews: some View {
+      GroupListCell(group: ModelFactory().group())
+    }
+}
