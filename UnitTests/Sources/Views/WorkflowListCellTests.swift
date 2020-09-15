02c8f5aee9166e1e5c122d12a7fa2be246ac1d28
Vadym Markov <markov.vadym@gmail.com>
2020-09-16 01:53:47 +0200

    Generate new snapshots

--- UnitTests/Sources/Views/WorkflowListCellTests.swift
+++ UnitTests/Sources/Views/WorkflowListCellTests.swift
@@ -3,6 +3,9 @@ import XCTest
 
 class WorkflowListCellTests: XCTestCase {
   func testSnapshotPreviews() {
-    assertPreview(from: WorkflowListCell_Previews.self, size: CGSize(width: 200, height: 40))
+    assertPreview(
+      from: WorkflowListCell_Previews.self,
+      size: CGSize(width: 300, height: 40)
+    )
   }
 }
