3258060ed46073da76c47392c89299af8ab3b225
Vadym Markov <markov.vadym@gmail.com>
2020-09-16 01:53:47 +0200

    Generate new snapshots

--- UnitTests/Sources/Views/GroupListCellTests.swift
+++ UnitTests/Sources/Views/GroupListCellTests.swift
@@ -3,6 +3,9 @@ import XCTest
 
 class GroupListCellTests: XCTestCase {
   func testSnapshotPreviews() {
-    assertPreview(from: GroupListCell_Previews.self, size: CGSize(width: 200, height: 40))
+    assertPreview(
+      from: GroupListCell_Previews.self,
+      size: CGSize(width: 300, height: 40)
+    )
   }
 }
