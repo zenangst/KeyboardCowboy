d4d05215b837f331786a7419c2c9f0e86cd82c92
Christoffer Winterkvist <christoffer@winterkvist.com>
2020-09-15 16:20:38 +0200

    Feature storage controller
    Add `StorageController` for saving groups to disk using
    JSON encoding & decoding

---
+++ LogicFramework/Sources/Controllers/StorageController.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+public protocol StorageControlling {
+  func load() throws ->  [Group]
+  func save(_ groups: [Group]) throws
+}
+
+enum StorageControllingError: Error {
+  case fileToLoadData
+}
+
+class StorageController: StorageControlling {
+  var path: String
+  var fileName: String
+
+  init(path: String, fileName: String) {
+    self.path = path
+    self.fileName = fileName
+  }
+
+  func load() throws -> [Group] {
+    let fileUrl = URL(fileURLWithPath: path).appendingPathComponent(fileName)
+    let decoder = JSONDecoder()
+    let fileManager = FileManager()
+
+    guard let data = fileManager.contents(atPath: fileUrl.path) else {
+      throw StorageControllingError.fileToLoadData
+    }
+
+    return try decoder.decode([Group].self, from: data)
+  }
+
+  func save(_ groups: [Group]) throws {
+    let fileUrl = URL(fileURLWithPath: path).appendingPathComponent(fileName)
+    let encoder = JSONEncoder()
+    encoder.outputFormatting = .prettyPrinted
+    let data = try encoder.encode(groups)
+    let fileManager = FileManager()
+    fileManager.createFile(atPath: fileUrl.path, contents: data, attributes: nil)
+  }
+}
