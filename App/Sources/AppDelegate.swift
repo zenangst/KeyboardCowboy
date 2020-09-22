cf7c8f35aa924f97578282c49c955d5629b722a3
Christoffer Winterkvist <christoffer@winterkvist.com>
2020-09-22 18:29:18 +0200

    Refactor AppDelegate
    - Add basic application delegate implementation to load groups from
      `~/.keyboard-cowboy.json`

--- App/Sources/AppDelegate.swift
+++ App/Sources/AppDelegate.swift
@@ -1,7 +1,21 @@
 import Cocoa
+import LogicFramework
 
 class AppDelegate: NSObject, NSApplicationDelegate {
+  var controller: CoreControlling?
+  let factory = ControllerFactory()
+
   func applicationDidFinishLaunching(_ notification: Notification) {
-    Swift.print("Hello world!")
+    let storageController = factory.storageController(path: "~", fileName: ".keyboard-cowboy.json")
+    do {
+      let groups = try storageController.load()
+      let groupsController = factory.groupsController(groups: groups)
+      let controller = factory.coreController(
+        groupsController: groupsController
+      )
+      self.controller = controller
+    } catch let error {
+      assertionFailure(error.localizedDescription)
+    }
   }
 }
