987fb141dc34074774a5df5abd494d29fff427af
Christoffer Winterkvist <christoffer@winterkvist.com>
2020-10-10 16:45:08 +0200

    Feature application main menu
    - Add main menu implementation
    - Fix crash in `MainWindow` by setting `isReleasedWhenClosed` to `false`
    - Refactor application delegate to handle opening and re-opening the
      application window.
    - Replace `runWindowless` with `openWindowAtLaunch` property

--- App/Sources/Application/main.swift
+++ App/Sources/Application/main.swift
@@ -1,4 +1,6 @@
 import Cocoa
 let appDelegate = AppDelegate()
+let mainMenu = AppMenu(title: "MainMenu")
 NSApplication.shared.delegate = appDelegate
+NSApplication.shared.mainMenu = mainMenu
 _ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
