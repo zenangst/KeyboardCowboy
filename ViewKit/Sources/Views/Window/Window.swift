e27270c71f5384303ab956d71241fa8093d55fa6
Christoffer Winterkvist <christoffer@winterkvist.com>
2020-10-28 18:56:08 +0100

    Remove uppercase I in Window filename

---
+++ ViewKit/Sources/Views/Window/Window.swift
@@ -0,0 +1,33 @@
+import Cocoa
+import SwiftUI
+
+public class Window: NSWindow {
+  var onClose: () -> Void
+
+  public init(
+    autosaveName: String,
+    toolbar: Toolbar? = nil,
+    onClose: @escaping () -> Void) {
+    self.onClose = onClose
+    let contentRect: CGRect = .init(origin: .zero, size: .init(width: 960, height: 480))
+    let styleMask: NSWindow.StyleMask = [
+      .titled, .closable, .miniaturizable,
+      .fullSizeContentView, .unifiedTitleAndToolbar, .resizable]
+    super.init(contentRect: contentRect,
+               styleMask: styleMask,
+               backing: .buffered,
+               defer: true)
+    self.setFrameAutosaveName(frameAutosaveName)
+    self.toolbar = toolbar
+    self.titlebarAppearsTransparent = true
+    self.isReleasedWhenClosed = false
+  }
+
+  public override var canBecomeKey: Bool { true }
+  public override var canBecomeMain: Bool { true }
+
+  public override func close() {
+    super.close()
+    onClose()
+  }
+}
