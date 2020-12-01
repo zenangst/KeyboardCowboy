4462b5f7c2b18d9ae532ae05bbe949a637af14d5
Christoffer Winterkvist <christoffer@winterkvist.com>
2020-12-01 20:33:49 +0100

    Add additional paragraph about re-enabling permissions

--- LogicFramework/Sources/Permissions/PermissionsController.swift
+++ LogicFramework/Sources/Permissions/PermissionsController.swift
@@ -14,6 +14,9 @@ final class PermissionsController: PermissionsControlling {
     To enable this, click on \"Open System Preferences\" on the dialog that just appeared.
 
     When the setting is enabled, restart \(applicationName) and you should be ready to go.
+
+    If you have already granted permission, try and disable and enable the current
+    entry inside the list of applications under "Allow the apps below to control your computer."
     """
   }
 
