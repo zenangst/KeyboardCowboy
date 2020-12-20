ea29a053877a6171fbb4bed9898a28c1471e7224
Christoffer Winterkvist <christoffer@winterkvist.com>
2020-12-20 19:45:44 +0100

    Fix highlighting issue in workflow list
    - Defer navigation destinations with the use of new `DeferView` to
      fix the highlighting issue that appears if you have nested navigation
      links

---
+++ ViewKit/Sources/Views/Shared/DeferedView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct DeferView<Content: View>: View {
+    let content: () -> Content
+
+    init(@ViewBuilder _ content: @escaping () -> Content) {
+        self.content = content
+    }
+    var body: some View {
+        content()
+    }
+}
