7a675aa3faa7f85a5b78b9dc0eaa35924837ef21
Christoffer Winterkvist <christoffer@winterkvist.com>
2022-03-02 15:29:47 +0100

    Refactor SidebarView by splitting handling of actions and sheets into functions

--- Sources/Views/Groups/SidebarView.swift
+++ Sources/Views/Groups/SidebarView.swift
@@ -20,37 +20,37 @@ struct SidebarView: View {
 
   var body: some View {
     WorkflowGroupListView(
-      appStore: appStore,
-      groupStore: groupStore,
-      selection: Binding<Set<String>>(get: {
-        selection
-      }, set: {
-        selection = $0
-        focus = .main(.groupComponent)
-      }),
-      action: { action in
-      switch action {
-      case .edit(let group):
-        sheet = .edit(group)
-      case .delete(let group):
-        groupStore.remove(group)
-      }
-    })
-    .sheet(item: $sheet) { sheet in
-      switch sheet {
-      case .edit(let group):
-        EditWorfklowGroupView(applicationStore: appStore, group: group) { action in
-          self.sheet = nil
-          switch action {
-          case .ok(let group):
-            groupStore.receive([group])
-          case .cancel:
-            break
-          }
+      appStore: appStore, groupStore: groupStore,
+      selection: $selection, action: handleAction(_:))
+    .sheet(item: $sheet, content: handleSheet(_:))
+    .focused($focus, equals: .sidebar)
+  }
+
+  // MARK: Private methods
+
+  private func handleAction(_ action: WorkflowGroupListView.Action) {
+    switch action {
+    case .edit(let group):
+      sheet = .edit(group)
+    case .delete(let group):
+      groupStore.remove(group)
+    }
+  }
+
+  @ViewBuilder
+  private func handleSheet(_ sheet: SidebarView.Sheet) -> some View {
+    switch sheet {
+    case .edit(let group):
+      EditWorfklowGroupView(applicationStore: appStore, group: group) { action in
+        self.sheet = nil
+        switch action {
+        case .ok(let group):
+          groupStore.receive([group])
+        case .cancel:
+          break
         }
       }
     }
-    .focused($focus, equals: .sidebar)
   }
 }
 
