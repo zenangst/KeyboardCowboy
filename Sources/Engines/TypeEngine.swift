a75026e7bbfaef4746c7bc74b0f60f212064d561
Christoffer Winterkvist <christoffer@winterkvist.com>
2022-05-06 22:08:46 +0200

    Fix indentation

--- Sources/Engines/TypeEngine.swift
+++ Sources/Engines/TypeEngine.swift
@@ -2,56 +2,56 @@ import Carbon
 import Foundation
 
 final class TypeEngine {
-  private let keyboardEngine: KeyboardEngine
-  private let store: KeyCodeStore
+    private let keyboardEngine: KeyboardEngine
+    private let store: KeyCodeStore
 
-  internal init(keyboardEngine: KeyboardEngine, store: KeyCodeStore) {
-    self.keyboardEngine = keyboardEngine
-    self.store = store
-  }
-
-  func run(_ command: TypeCommand) async throws {
-    let input = command.input
-    let uppercaseLetters = CharacterSet.uppercaseLetters
-    let newLines = CharacterSet.newlines
-
-    for character in input {
-      let string = String(character)
-      let charSet = CharacterSet(charactersIn: string)
-      var modifiers: [ModifierKey] = .init()
-      var key: String = string
-
-      if let container = store.stringWithModifier(for: string),
-         let rawValue = store.string(for: container.keyCode) {
-        key = rawValue
-
-        switch container.modifier {
-        case .clear:
-          modifiers = []
-        case .option:
-          modifiers = [.option]
-        case .shift:
-          modifiers = [.shift]
-        case .shiftOption:
-          modifiers = [.shift, .option]
-        }
-
-        if charSet.isSubset(of: uppercaseLetters) {
-          modifiers = [.shift]
-          key = string
-        }
-      } else {
-        if charSet.isSubset(of: newLines) {
-          modifiers = []
-          key = KeyCodes.specialKeys[kVK_Return]!
-        } else if charSet.isSubset(of: uppercaseLetters) {
-          modifiers = [.shift]
-        }
-      }
-
-      let keyboardShortcut = KeyShortcut(key: key, modifiers: modifiers)
-      let command = KeyboardCommand(keyboardShortcut: keyboardShortcut)
-      try keyboardEngine.run(command, type: .keyDown, with: nil)
+    internal init(keyboardEngine: KeyboardEngine, store: KeyCodeStore) {
+        self.keyboardEngine = keyboardEngine
+        self.store = store
     }
-  }
+
+    func run(_ command: TypeCommand) async throws {
+        let input = command.input
+        let uppercaseLetters = CharacterSet.uppercaseLetters
+        let newLines = CharacterSet.newlines
+
+        for character in input {
+            let string = String(character)
+            let charSet = CharacterSet(charactersIn: string)
+            var modifiers: [ModifierKey] = .init()
+            var key: String = string
+
+            if let container = store.stringWithModifier(for: string),
+               let rawValue = store.string(for: container.keyCode) {
+                key = rawValue
+
+                switch container.modifier {
+                case .clear:
+                    modifiers = []
+                case .option:
+                    modifiers = [.option]
+                case .shift:
+                    modifiers = [.shift]
+                case .shiftOption:
+                    modifiers = [.shift, .option]
+                }
+
+                if charSet.isSubset(of: uppercaseLetters) {
+                    modifiers = [.shift]
+                    key = string
+                }
+            } else {
+                if charSet.isSubset(of: newLines) {
+                    modifiers = []
+                    key = KeyCodes.specialKeys[kVK_Return]!
+                } else if charSet.isSubset(of: uppercaseLetters) {
+                    modifiers = [.shift]
+                }
+            }
+
+            let keyboardShortcut = KeyShortcut(key: key, modifiers: modifiers)
+            let command = KeyboardCommand(keyboardShortcut: keyboardShortcut)
+            try keyboardEngine.run(command, type: .keyDown, with: nil)
+        }
+    }
 }
