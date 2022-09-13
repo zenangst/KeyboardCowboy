6e12723606fa722836e74c3dfabfbfa616ef4e49
Christoffer Winterkvist <christoffer@winterkvist.com>
2022-09-13 20:35:22 +0200

    Update to Apps 1.2.1 to fix Safari being missing on macOS Ventura

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -3,7 +3,7 @@ import ProjectDescription
 let dependencies = Dependencies(
     swiftPackageManager: [
       .remote(url: "https://github.com/krzysztofzablocki/Inject.git", requirement: .exact("1.1.0")),
-      .remote(url: "https://github.com/zenangst/Apps.git", requirement: .exact("1.2.0")),
+      .remote(url: "https://github.com/zenangst/Apps.git", requirement: .exact("1.2.1")),
       .remote(url: "https://github.com/zenangst/InputSources.git", requirement: .exact("1.0.0")),
       .remote(url: "https://github.com/zenangst/KeyCodes.git", requirement: .exact("4.0.0")),
       .remote(url: "https://github.com/zenangst/LaunchArguments.git", requirement: .exact("1.0.0")),
