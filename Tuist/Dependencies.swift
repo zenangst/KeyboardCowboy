f72cdae1ebb391cbd12cd3a5cbec6555d9eea6bf
Christoffer Winterkvist <christoffer@winterkvist.com>
2022-10-31 18:36:34 +0100

    Update dependencies

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -3,11 +3,11 @@ import ProjectDescription
 let dependencies = Dependencies(
     swiftPackageManager: [
       .remote(url: "https://github.com/krzysztofzablocki/Inject.git", requirement: .exact("1.1.0")),
-      .remote(url: "https://github.com/zenangst/Apps.git", requirement: .exact("1.2.1")),
-      .remote(url: "https://github.com/zenangst/InputSources.git", requirement: .exact("1.0.0")),
-      .remote(url: "https://github.com/zenangst/KeyCodes.git", requirement: .exact("4.0.0")),
-      .remote(url: "https://github.com/zenangst/LaunchArguments.git", requirement: .exact("1.0.0")),
-      .remote(url: "https://github.com/zenangst/MachPort.git", requirement: .exact("1.1.0")),
+      .remote(url: "https://github.com/zenangst/Apps.git", requirement: .exact("1.3.0")),
+      .remote(url: "https://github.com/zenangst/InputSources.git", requirement: .exact("1.0.1")),
+      .remote(url: "https://github.com/zenangst/KeyCodes.git", requirement: .exact("4.0.1")),
+      .remote(url: "https://github.com/zenangst/LaunchArguments.git", requirement: .exact("1.0.1")),
+      .remote(url: "https://github.com/zenangst/MachPort.git", requirement: .exact("1.1.1")),
     ],
     platforms: [.macOS]
 )
