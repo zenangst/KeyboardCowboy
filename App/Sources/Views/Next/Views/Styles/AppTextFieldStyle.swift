a1a01ac86cb8a52ceccccd739b4121deb9442090
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-01-05 19:28:24 +0100

    Tweak the alpha values in the AppTextFieldStyle

--- App/Sources/Views/Next/Views/Styles/AppTextFieldStyle.swift
+++ App/Sources/Views/Next/Views/Styles/AppTextFieldStyle.swift
@@ -10,9 +10,11 @@ struct AppTextFieldStyle: TextFieldStyle {
       .padding(2.5)
       .background(
         RoundedRectangle(cornerRadius: 4)
-          .stroke(Color(isFocused ? .controlAccentColor : .windowFrameTextColor), lineWidth: 1)
-          .opacity(isFocused ? 0.75 : isHovered ? 0.25 : 0)
+          .stroke(Color(isFocused ? .controlAccentColor.withAlphaComponent(0.5) : .windowFrameTextColor), lineWidth: 1)
+          .opacity(isFocused ? 0.75 : isHovered ? 0.15 : 0)
       )
+      .shadow(color: isFocused ? .accentColor.opacity(0.8) : Color(.sRGBLinear, white: 0, opacity: 0.33),
+              radius: isFocused ? 1.0 : 0.0)
       .onHover(perform: { newValue in  withAnimation { isHovered = newValue } })
       .font(.body)
       .bold()
