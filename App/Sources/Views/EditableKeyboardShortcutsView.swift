7b8a3e20de93cfbcd227923ff5cdd046ffed4324
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-01-24 19:23:56 +0100

    Scroll to placeholder keyboard shortcut when adding a new one

--- App/Sources/Views/EditableKeyboardShortcutsView.swift
+++ App/Sources/Views/EditableKeyboardShortcutsView.swift
@@ -17,103 +17,16 @@ struct EditableKeyboardShortcutsView: View {
   private let animation: Animation = .easeOut(duration: 0.2)
 
   var body: some View {
-    HStack {
-      ScrollView(.horizontal) {
-        EditableStack(
-          $keyboardShortcuts,
-          axes: .horizontal,
-          selectedColor: $selectedColor,
-          onClick: { id, index in
-            if replacing == id {
-              record()
-            }
-            replacing = id
-          },
-          onMove: { keyboardShortcuts.move(fromOffsets: $0, toOffset: $1) },
-          onDelete: { offsets in
-            withAnimation(animation) {
-              keyboardShortcuts.remove(atOffsets: offsets)
-            }
-          },
-          content: { keyboardShortcut in
-            HStack(spacing: 6) {
-              ForEach(keyboardShortcut.wrappedValue.modifiers) { modifier in
-                ModifierKeyIcon(
-                  key: modifier,
-                  alignment: keyboardShortcut.wrappedValue.lhs
-                  ? modifier == .shift ? .bottomLeading : .topTrailing
-                  : modifier == .shift ? .bottomTrailing : .topLeading
-                )
-                  .frame(minWidth: modifier == .command || modifier == .shift ? 44 : 32, minHeight: 32)
-                  .fixedSize(horizontal: true, vertical: true)
-              }
-              RegularKeyIcon(letter: keyboardShortcut.wrappedValue.key, width: 32, height: 32)
-                .fixedSize(horizontal: true, vertical: true)
-            }
-            .contextMenu {
-              Text(keyboardShortcut.wrappedValue.validationValue)
-              Divider()
-              Button("Re-record") {
-                replacing = keyboardShortcut.id
-                record()
-              }
-              Button(action: {
-                if let index = keyboardShortcuts.firstIndex(where: { $0.id == keyboardShortcut.id }) {
-                  _ = withAnimation(animation) {
-                    keyboardShortcuts.remove(at: index)
-                  }
-                }
-              }, label: {
-                Text("Remove")
-              })
-            }
-            .padding(4)
-            .background(
-              RoundedRectangle(cornerRadius: 8, style: .continuous)
-                .stroke(Color(.disabledControlTextColor))
-                .opacity(0.5)
-            )
-          })
-        .padding(4)
-      }
-      Spacer()
-      Button(action: {
-        state = .recording
-        recorderStore.mode = .record
-        let keyShortcut = KeyShortcut(id: placeholderId, key: "Recording ...", lhs: true)
-        replacing = keyShortcut.id
-        keyboardShortcuts.append(keyShortcut)
-        withAnimation(animation) {
-          isGlowing = true
-          selectedColor = Color(.systemRed)
+    ScrollViewReader { proxy in
+      HStack {
+        ScrollView(.horizontal) {
+          content()
         }
-      },
-             label: { Image(systemName: "plus").frame(width: 10, height: 10) })
-        .buttonStyle(.gradientStyle(config: .init(nsColor: .systemGreen, grayscaleEffect: true)))
-        .padding(.trailing, 4)
+        Spacer()
+        addButton(proxy)
+      }
     }
-    .overlay(
-      ZStack {
-        if keyboardShortcuts.isEmpty {
-          HStack {
-            Spacer()
-            Text("Press the plus (+) button to record a keyboard shortcut")
-              .font(.footnote)
-            Spacer()
-          }
-          .padding(4)
-        }
-        if state == .recording {
-          RoundedRectangle(cornerRadius: 4)
-            .stroke(isGlowing
-                    ? Color(.systemRed) .opacity(0.5)
-                    : Color.clear, lineWidth: 1)
-            .animation(Animation
-              .easeInOut(duration: 1.25)
-              .repeatForever(autoreverses: true), value: isGlowing)
-        }
-      }
-    )
+    .overlay(overlay())
     .onChange(of: controlActiveState, perform: { value in
       if value != .key {
         reset()
@@ -138,6 +51,110 @@ struct EditableKeyboardShortcutsView: View {
       }
     })
     .enableInjection()
+  }
+
+  private func content() -> some View {
+    EditableStack(
+      $keyboardShortcuts,
+      axes: .horizontal,
+      selectedColor: $selectedColor,
+      onClick: { id, index in
+        if replacing == id {
+          record()
+        }
+        replacing = id
+      },
+      onMove: { keyboardShortcuts.move(fromOffsets: $0, toOffset: $1) },
+      onDelete: { offsets in
+        withAnimation(animation) {
+          keyboardShortcuts.remove(atOffsets: offsets)
+        }
+      },
+      content: { keyboardShortcut in
+        HStack(spacing: 6) {
+          ForEach(keyboardShortcut.wrappedValue.modifiers) { modifier in
+            ModifierKeyIcon(
+              key: modifier,
+              alignment: keyboardShortcut.wrappedValue.lhs
+              ? modifier == .shift ? .bottomLeading : .topTrailing
+              : modifier == .shift ? .bottomTrailing : .topLeading
+            )
+            .frame(minWidth: modifier == .command || modifier == .shift ? 44 : 32, minHeight: 32)
+            .fixedSize(horizontal: true, vertical: true)
+          }
+          RegularKeyIcon(letter: keyboardShortcut.wrappedValue.key, width: 32, height: 32)
+            .fixedSize(horizontal: true, vertical: true)
+        }
+        .contextMenu {
+          Text(keyboardShortcut.wrappedValue.validationValue)
+          Divider()
+          Button("Re-record") {
+            replacing = keyboardShortcut.id
+            record()
+          }
+          Button("Remove") {
+            if let index = keyboardShortcuts.firstIndex(where: { $0.id == keyboardShortcut.id }) {
+              _ = withAnimation(animation) {
+                keyboardShortcuts.remove(at: index)
+              }
+            }
+          }
+        }
+        .padding(4)
+        .background(
+          RoundedRectangle(cornerRadius: 8, style: .continuous)
+            .stroke(Color(.disabledControlTextColor))
+            .opacity(0.5)
+        )
+        .id(keyboardShortcut.id)
+      })
+    .padding(4)
+  }
+
+  private func overlay() -> some View {
+    ZStack {
+      if keyboardShortcuts.isEmpty {
+        HStack {
+          Spacer()
+          Text("Press the plus (+) button to record a keyboard shortcut")
+            .font(.footnote)
+          Spacer()
+        }
+        .padding(4)
+      }
+      if state == .recording {
+        RoundedRectangle(cornerRadius: 4)
+          .stroke(isGlowing
+                  ? Color(.systemRed) .opacity(0.5)
+                  : Color.clear, lineWidth: 1)
+          .animation(Animation
+            .easeInOut(duration: 1.25)
+            .repeatForever(autoreverses: true), value: isGlowing)
+      }
+    }
+  }
+
+  private func addButton(_ proxy: ScrollViewProxy) -> some View {
+    Button(action: {
+      state = .recording
+      recorderStore.mode = .record
+      let keyShortcut = KeyShortcut(id: placeholderId, key: "Recording ...", lhs: true)
+      replacing = keyShortcut.id
+      keyboardShortcuts.append(keyShortcut)
+      withAnimation(animation) {
+        isGlowing = true
+        selectedColor = Color(.systemRed)
+        DispatchQueue.main.async {
+          withAnimation(animation) {
+            proxy.scrollTo(keyShortcut.id)
+          }
+        }
+      }
+    },
+           label: { Image(systemName: "plus").frame(width: 10, height: 10) })
+    .buttonStyle(.gradientStyle(config: .init(nsColor: .systemGreen, grayscaleEffect: true)))
+    .padding(.trailing, 4)
+    .disabled(state == .recording)
   }
 
   private func record() {
