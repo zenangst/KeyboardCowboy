cee70b20c068b2bf1b0d133b2f627a8dab7d6eaf
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-01-25 16:57:22 +0100

    Remove Group from WorkflowTriggerListView

--- App/Sources/Views/WorkflowTriggerListView.swift
+++ App/Sources/Views/WorkflowTriggerListView.swift
@@ -10,51 +10,49 @@ struct WorkflowTriggerListView: View {
   }
 
   var body: some View {
-    Group {
-      switch model.trigger {
-      case .keyboardShortcuts(let shortcuts):
-        HStack {
-          Button(action: {
-            onAction(.removeTrigger(workflowId: model.id))
-          },
-                 label: { Image(systemName: "xmark") })
-          .buttonStyle(.appStyle)
-          Label("Keyboard Shortcuts sequence:", image: "")
-            .padding(.trailing, 12)
-        }
+    switch model.trigger {
+    case .keyboardShortcuts(let shortcuts):
+      HStack {
+        Button(action: {
+          onAction(.removeTrigger(workflowId: model.id))
+        },
+               label: { Image(systemName: "xmark") })
+        .buttonStyle(.appStyle)
+        Label("Keyboard Shortcuts sequence:", image: "")
+          .padding(.trailing, 12)
+      }
+      .padding([.leading, .trailing], 8)
+      WorkflowShortcutsView(shortcuts) { keyboardShortcuts in
+        onAction(.updateKeyboardShortcuts(workflowId: model.id, keyboardShortcuts: keyboardShortcuts))
+      }
+    case .applications(let triggers):
+      HStack {
+        Button(action: {
+          onAction(.removeTrigger(workflowId: model.id))
+        },
+               label: { Image(systemName: "xmark") })
+        .buttonStyle(.gradientStyle(config: .init(nsColor: .systemRed, grayscaleEffect: true)))
+        Label("Application trigger:", image: "")
+      }
+      .padding([.leading, .trailing], 8)
+      WorkflowApplicationTriggerView(triggers) { action in
+        onAction(.applicationTrigger(workflowId: model.id, action: action))
+      }
+      .padding(.bottom, 16)
+    case .none:
+      Label("Add a trigger:", image: "")
         .padding([.leading, .trailing], 8)
-        WorkflowShortcutsView(shortcuts) { keyboardShortcuts in
-          onAction(.updateKeyboardShortcuts(workflowId: model.id, keyboardShortcuts: keyboardShortcuts))
-        }
-      case .applications(let triggers):
-        HStack {
-          Button(action: {
-            onAction(.removeTrigger(workflowId: model.id))
-          },
-                 label: { Image(systemName: "xmark") })
-          .buttonStyle(.gradientStyle(config: .init(nsColor: .systemRed, grayscaleEffect: true)))
-          Label("Application trigger:", image: "")
-        }
-        .padding([.leading, .trailing], 8)
-        WorkflowApplicationTriggerView(triggers) { action in
-          onAction(.applicationTrigger(workflowId: model.id, action: action))
-        }
-        .padding(.bottom, 16)
-      case .none:
-        Label("Add a trigger:", image: "")
-          .padding([.leading, .trailing], 8)
-        WorkflowTriggerView(onAction: { action in
-          onAction(.trigger(workflowId: model.id, action: action))
-        })
+      WorkflowTriggerView(onAction: { action in
+        onAction(.trigger(workflowId: model.id, action: action))
+      })
 
-        HStack {
-          Spacer()
-          Text("Choose if you want to bind this workflow to an application or assign it a global keyboard shortcut sequence.")
-            .multilineTextAlignment(.center)
-            .frame(alignment: .center)
-            .font(.caption)
-          Spacer()
-        }
+      HStack {
+        Spacer()
+        Text("Choose if you want to bind this workflow to an application or assign it a global keyboard shortcut sequence.")
+          .multilineTextAlignment(.center)
+          .frame(alignment: .center)
+          .font(.caption)
+        Spacer()
       }
     }
   }
