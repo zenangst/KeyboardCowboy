040ea234628c52b98fa558c2cc1bcf4bda3b7e84
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-04-04 07:54:29 +0200

    Refactor OpenURLSwapTabsPlugin to rely on accessibility lookup instead of Apple Script

--- App/Sources/Engines/Open/Plugins/OpenURLSwapTabsPlugin.swift
+++ App/Sources/Engines/Open/Plugins/OpenURLSwapTabsPlugin.swift
@@ -1,3 +1,5 @@
+import AXEssibility
+import Windows
 import Cocoa
 
 final class OpenURLSwapTabsPlugin {
@@ -5,6 +7,7 @@ final class OpenURLSwapTabsPlugin {
     case failedToCreate
     case failedToCompile
     case failedToRun
+    case couldFindOpenUrl
   }
 
   private let engine: ScriptEngine
@@ -14,36 +17,27 @@ final class OpenURLSwapTabsPlugin {
   }
 
   func execute(_ command: OpenCommand) async throws {
-    let source = """
-      set searchPattern to "\(command.path)"
-      tell application "Safari"
-        repeat with cWindow in windows
-          repeat with cTab in tabs of cWindow
-            set currentIndex to index of cTab
-            set currentURL to URL of cTab
-            if currentURL contains searchPattern then
-              set index of cWindow to 1
-              set current tab of cWindow to cTab
-              set visible of cWindow to true
-              tell application "System Events" to tell process "Safari"
-                perform action "AXRaise" of window 1
-                do shell script "open -j -a Safari"
-              end tell
-              delay 0.2
-              set current tab of cWindow to cTab
-              set visible of cWindow to true
-              return true
-              exit repeat
-            end if
-          end repeat
-        end repeat
-        error "Unable to find tab"
-      end tell
-      """
-    let script = ScriptCommand.appleScript(
-      id: "OpenURLSwapTabsPlugin",
-      isEnabled: true, name: "Safari Swap Tabs",
-      source: .inline(source))
-    _ = try await engine.run(script)
+    let bundleIdentifier = command.application?.bundleIdentifier ?? "com.apple.Safari"
+    if let runningApplication = NSWorkspace.shared.runningApplications
+      .first(where: { $0.bundleIdentifier == bundleIdentifier })
+    {
+      let axApp = AppAccessibilityElement(runningApplication.processIdentifier)
+      let windows = try axApp.windows()
+      var success: Bool = false
+
+      for window in windows {
+        if let url: URL = window.findAttribute(.url, of: "AXWebArea"),
+            url.absoluteString.contains(command.path) {
+          window.performAction(.raise)
+          success = true
+          runningApplication.activate(options: .activateIgnoringOtherApps)
+          break
+        }
+      }
+
+      if !success {
+        throw OpenURLSwapToPluginError.couldFindOpenUrl
+      }
+    }
   }
 }
