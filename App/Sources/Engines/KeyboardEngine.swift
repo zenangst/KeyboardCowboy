6d13886b2eaf246082ad273b2fb8d19b99a7d21c
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-04-11 20:19:52 +0200

    Improve arrow key navigation in the keyboard engine

--- App/Sources/Engines/KeyboardEngine.swift
+++ App/Sources/Engines/KeyboardEngine.swift
@@ -35,6 +35,15 @@ final class KeyboardEngine {
       var flags = CGEventFlags()
       keyboardShortcut.modifiers.forEach { flags.insert($0.cgModifierFlags) }
       do {
+        var flags = flags
+        // In applications like Xcode, we need to set the numeric pad flag for
+        // the application to properly respond to arrow key events as if the
+        // user used the actual arrow keys to navigate.
+        let arrows = 123...126
+        if arrows.contains(key) {
+          flags.insert(.maskNumericPad)
+        }
+
         try machPort.post(key, type: type, flags: flags) { newEvent in
           if let originalEvent {
             let originalKeyboardEventAutorepeat = originalEvent.getIntegerValueField(.keyboardEventAutorepeat)
