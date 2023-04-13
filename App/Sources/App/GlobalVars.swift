5aa3f8743546e969ae72c3ac8554a0c21eeae934
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-04-13 20:39:21 +0200

    Add global missionControlIsActive function

--- App/Sources/App/GlobalVars.swift
+++ App/Sources/App/GlobalVars.swift
@@ -1,5 +1,20 @@
 import Foundation
 import LaunchArguments
+import CoreGraphics
 
 let isRunningPreview = ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] != nil
 let launchArguments = LaunchArgumentsController<LaunchArgument>()
+
+func missionControlIsActive() -> Bool {
+  let windows = CGWindowListCopyWindowInfo([.optionOnScreenOnly, .excludeDesktopElements], kCGNullWindowID) as [AnyObject]? ?? []
+  return !windows.filter { entry in
+    guard let appName = entry[kCGWindowOwnerName as String] as? String,
+          let layer = entry[kCGWindowLayer as String] as? Int,
+          appName == "Dock" &&
+          layer == CGWindowLevelKey.desktopIconWindow.rawValue else {
+      return false
+    }
+
+    return true
+  }.isEmpty
+}
