788bd7ac7af4e16ee192067de489f2495e9ada2e
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-04-16 18:58:46 +0200

    Fix dependencies in tuist

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -4,14 +4,15 @@ let dependencies = Dependencies(
     swiftPackageManager: [
       .remote(url: "https://github.com/krzysztofzablocki/Inject.git", requirement: .exact("1.1.0")),
       .remote(url: "https://github.com/zenangst/Apps.git", requirement: .exact("1.3.1")),
+      .remote(url: "https://github.com/zenangst/AXEssibility.git", requirement: .exact("0.0.4")),
       .remote(url: "https://github.com/zenangst/Dock.git", requirement: .exact("1.0.0")),
       .remote(url: "https://github.com/zenangst/InputSources.git", requirement: .exact("1.0.1")),
       .remote(url: "https://github.com/zenangst/KeyCodes.git", requirement: .exact("4.0.1")),
       .remote(url: "https://github.com/zenangst/LaunchArguments.git", requirement: .exact("1.0.1")),
-//      .remote(url: "https://github.com/zenangst/MachPort.git", requirement: .exact("3.0.1")),
+      .remote(url: "https://github.com/zenangst/MachPort.git", requirement: .exact("3.0.1")),
       .remote(url: "https://github.com/zenangst/Windows.git", requirement: .exact("1.0.0")),
-      .local(path: "../AXEssibility"),
-      .local(path: "../MachPort"),
+//      .local(path: "../AXEssibility"),
+//      .local(path: "../MachPort"),
     ],
     platforms: [.macOS]
 )
