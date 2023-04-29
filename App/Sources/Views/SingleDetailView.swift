81f2bdbc2e407cd703db6740847b3b6751b32308
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-04-29 19:34:49 +0200

    Add gradient to single detail views header

--- App/Sources/Views/SingleDetailView.swift
+++ App/Sources/Views/SingleDetailView.swift
@@ -56,7 +56,14 @@ struct SingleDetailView: View {
         .padding(.bottom, 32)
         .background(alignment: .bottom, content: {
           Rectangle()
-            .fill(Color(nsColor: .windowBackgroundColor))
+            .fill(
+              LinearGradient(stops: [
+                .init(color: Color(nsColor: .windowBackgroundColor.blended(withFraction: 0.3, of: .white)!), location: 0.0),
+                .init(color: Color(nsColor: .windowBackgroundColor.blended(withFraction: 0.1, of: .white)!), location: 0.01),
+                .init(color: Color(nsColor: .windowBackgroundColor), location: 0.8),
+                .init(color: Color(nsColor: .windowBackgroundColor.blended(withFraction: 0.3, of: .black)!), location: 1.0),
+              ], startPoint: .top, endPoint: .bottom)
+            )
             .mask(
               Canvas(rendersAsynchronously: true) { context, size in
                 context.fill(
