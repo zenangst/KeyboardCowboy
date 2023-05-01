a84a4e31512950034aa8a346db388c84ee0c2a9a
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-05-01 18:52:13 +0200

    Remove unused `responderId`

--- App/Sources/Stores/AppStorageStore.swift
+++ App/Sources/Stores/AppStorageStore.swift
@@ -6,12 +6,9 @@ struct AppStorageStore {
   @AppStorage("selectedConfiguration", store: UserDefaults(suiteName: "com.zenangst.Keyboard-Cowboy.debug")) var configId: String = ""
   @AppStorage("selectedGroupIds", store: UserDefaults(suiteName: "com.zenangst.Keyboard-Cowboy.debug")) var groupIds = Set<String>()
   @AppStorage("selectedWorkflowIds", store: UserDefaults(suiteName: "com.zenangst.Keyboard-Cowboy.debug")) var workflowIds = Set<String>()
-  @AppStorage("responderId", store: UserDefaults(suiteName: "com.zenangst.Keyboard-Cowboy.debug")) var responderId: String = ""
-
   #else
   @AppStorage("selectedConfiguration") var configId: String = ""
   @AppStorage("selectedGroupIds") var groupIds = Set<String>()
   @AppStorage("selectedWorkflowIds") var workflowIds = Set<String>()
-  @AppStorage("responderId") var responderId: String = ""
   #endif
 }
