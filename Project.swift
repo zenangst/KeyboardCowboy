7249e0c4f1820fa9add5b004d25f9c2512b50747
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-05-25 20:39:30 +0200

    Bump version number

--- Project.swift
+++ Project.swift
@@ -52,10 +52,10 @@ let project = Project(
                         "ASSETCATALOG_COMPILER_APPICON_NAME": "AppIcon",
                         "CODE_SIGN_IDENTITY": "Apple Development",
                         "CODE_SIGN_STYLE": "Automatic",
-                        "CURRENT_PROJECT_VERSION": "137",
+                        "CURRENT_PROJECT_VERSION": "138",
                         "DEVELOPMENT_TEAM": env["TEAM_ID"],
                         "ENABLE_HARDENED_RUNTIME": true,
-                        "MARKETING_VERSION": "3.0.0",
+                        "MARKETING_VERSION": "3.0.1",
                         "PRODUCT_NAME": "Keyboard Cowboy"
                     ],
                     configurations: [
