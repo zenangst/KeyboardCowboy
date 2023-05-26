ab5038071aaa95edf89a4cf8add700dc34a8607b
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-05-26 08:08:09 +0200

    Remove old tests
    Sadly, there was nothing to salvage here.
    Let's just start from scratch.

--- UnitTests/Sources/Mocks/NotificationControllerMock.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import LogicFramework
-
-class NotificationControllerMock: NotificationControlling {
-  func addObserver(_ observer: Any, selector aSelector: Selector,
-                   name aName: NSNotification.Name?,
-                   object anObject: Any?) {}
-}
