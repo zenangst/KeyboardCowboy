3a3c3053a21e217b4dde2d6bc0004a836cf84e90
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-06-01 21:39:18 +0200

    Rename KeyboardShortcutsCache to KeyboardShortcutsController
    - Add KeyboardShortcutsControllerTests for looking up keyboard shortcuts in a "large" collection
    - Add new `PartialMatch` type to `KeyboardShortcutsController` so that you can't just send any String
    - Increase performance of the `KeyboardShortcutsController.cache` method by dropping the use of `for-in`
      in favor of `forEach` to drop the control-flow overhead. In addition, we manage the offset count manually
      instead of using `.enumerated()`, which also boost the performance a tad in the right direction.

---
+++ UnitTests/Sources/Controllers/KeyboardShortcutsControllerTests.swift
@@ -0,0 +1,81 @@
+import XCTest
+@testable import Keyboard_Cowboy
+
+final class KeyboardShortcutsControllerTests: XCTestCase {
+  func testLookupInLargeCollection() {
+    let controller = KeyboardShortcutsController()
+    let groups = generateGroups(50, workflows: 50, keyboardShortcuts: 20)
+
+    controller.cache(groups)
+
+    var offset = 0
+    let initialKey = KeyShortcut(key: "group-id-9-workflow-keyboard-shortcut-key-0")
+    let result = recursiveLookup(controller, keyShortcut: initialKey, offset: &offset)
+    XCTAssertTrue(result)
+  }
+
+  // TODO: Add additional tests
+  // We should add tests that cover binding workflows
+  // that are bound to the frontmost application.
+
+
+  // MARK: - Private methods
+
+  private func recursiveLookup(_ controller: KeyboardShortcutsController,
+                               keyShortcut: KeyShortcut,
+                               offset: inout Int,
+                               partial: PartialMatch = .init(rawValue: ".")) -> Bool {
+    let result = controller.lookup(keyShortcut, partialMatch: partial)
+
+    switch result {
+    case .exact:
+      return true
+    case .partialMatch(let partial):
+      offset += 1
+      let key = removeLastEntry(from: keyShortcut.key) + "-\(offset)"
+      return recursiveLookup(controller,
+                             keyShortcut: .init(key: key),
+                             offset: &offset, partial: partial)
+    case .none:
+      return false
+    }
+  }
+
+  private func removeLastEntry(from string: String) -> String {
+    var components = string.components(separatedBy: "-")
+    components.removeLast()
+    return components.joined(separator: "-")
+  }
+
+  private func generateGroups(_ groups: Int,
+                              workflows: Int,
+                              keyboardShortcuts: Int) -> [WorkflowGroup] {
+    (0..<groups).map {
+      WorkflowGroup(id: "group-id-\($0)",
+                    name: "group-name-\($0)",
+                    workflows: generateWorkflows(
+                      workflows,
+                      keyboardShortcuts: keyboardShortcuts,
+                      prefix: "group-id-\($0)"))
+    }
+  }
+
+  private func generateWorkflows(_ number: Int,
+                                 keyboardShortcuts: Int,
+                                 prefix: String) -> [Workflow] {
+    (0..<number).map {
+      let newPrefix = "\(prefix)-workflow"
+      return Workflow(id: "\(newPrefix)-id-\($0)",
+                      name: "\(newPrefix)-name-\($0)",
+                      trigger: Workflow.Trigger.keyboardShortcuts(generateTriggers(keyboardShortcuts, prefix: newPrefix)))
+    }
+  }
+
+  private func generateTriggers(_ number: Int, prefix: String) -> [KeyShortcut] {
+    (0..<number).map {
+      let newPrefix = "\(prefix)-keyboard-shortcut"
+      return KeyShortcut(id: "\(newPrefix)-id-\($0)",
+                         key: "\(newPrefix)-key-\($0)")
+    }
+  }
+}
