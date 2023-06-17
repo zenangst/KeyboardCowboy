e84a01841ed3a9a592a4ae2cdf46af414fd2830d
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-06-17 12:31:51 +0200

    Implement migration for OldScriptCommand to ScriptCommand

--- App/Sources/Core/Models/Commands/ScriptCommand.swift
+++ App/Sources/Core/Models/Commands/ScriptCommand.swift
@@ -23,4 +23,42 @@ struct ScriptCommand: MetaDataProviding {
     self.meta = Command.MetaData(
       id: id, name: name, isEnabled: true, notification: notification)
   }
+
+  init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+
+    do {
+      self.kind = try container.decode(Kind.self, forKey: .kind)
+      self.source = try container.decode(Source.self, forKey: .source)
+      self.meta = try container.decode(Command.MetaData.self, forKey: .meta)
+    } catch {
+      let oldScript = try OldScriptCommand(from: decoder)
+
+      let id: String
+      let name: String
+      let isEnabled: Bool
+
+      switch oldScript {
+      case .appleScript(let _id, let _isEnabled, let _name, _):
+        self.kind = .appleScript
+        id = _id
+        name = _name ?? ""
+        isEnabled = _isEnabled
+      case .shell(let _id, let _isEnabled, let _name, _):
+        self.kind = .shellScript
+        id = _id
+        name = _name ?? ""
+        isEnabled = _isEnabled
+      }
+
+      switch oldScript.sourceType {
+      case .path(let path):
+        self.source = .path(path)
+      case .inline(let source):
+        self.source = .inline(source)
+      }
+
+      self.meta = Command.MetaData(id: id, name: name, isEnabled: isEnabled, notification: false)
+    }
+  }
 }
