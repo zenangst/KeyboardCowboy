a8a5bbd29458d16f59f9ea7d5cec9296c0a390ea
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-06-22 16:33:42 +0200

    Add -n to tuist generate

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 curl -Ls https://install.tuist.io | bash
-cd ..
-touch .env
-echo "APP_NAME=Keyboard Cowboy" >> .env
-echo "APP_SCHEME=Keyboard-Cowboy" >> .env
-echo "APP_BUNDLE_IDENTIFIER=com.zenangst.Keyboard-Cowboy" >> .env
+touch ../.env
+echo "APP_NAME=Keyboard Cowboy" >> ../.env
+echo "APP_SCHEME=Keyboard-Cowboy" >> ../.env
+echo "APP_BUNDLE_IDENTIFIER=com.zenangst.Keyboard-Cowboy" >> ../.env
 echo "TEAM_ID=XXXXXXXXXX" >> .env
-source .env
-tuist fetch
-tuist generate
+source ../.env
+tuist fetch --path ../
+tuist generate -n --path ../
