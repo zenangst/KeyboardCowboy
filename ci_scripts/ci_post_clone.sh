8a5e1ffe8d867e28390a587eb5393923bbf0fdc1
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-06-22 16:15:24 +0200

    Use cd .. instead of hardcoding paths

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 curl -Ls https://install.tuist.io | bash
-touch .ci_env
+cd ..
+touch .env
 echo "APP_NAME=Keyboard Cowboy" >> .env
 echo "APP_SCHEME=Keyboard-Cowboy" >> .env
 echo "APP_BUNDLE_IDENTIFIER=com.zenangst.Keyboard-Cowboy" >> .env
 echo "TEAM_ID=XXXXXXXXXX" >> .env
 source .env
-tuist fetch --path ../
-tuist generate --path ../
+tuist fetch
+tuist generate
