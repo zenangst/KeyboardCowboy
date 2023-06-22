7fcb0ca8d217571d290f4b022d5f65430a2ff005
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-06-22 15:46:11 +0200

    Add ci_post_clone script

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+brew install tuist
+touch .env
+echo "APP_NAME=Keyboard Cowboy" >> .env
+echo "APP_SCHEME=Keyboard-Cowboy" >> .env
+echo "APP_BUNDLE_IDENTIFIER=com.zenangst.Keyboard-Cowboy" >> .env
+echo "TEAM_ID=XXXXXXXXXX" >> .env
+tuist fetch
+tuist generate
