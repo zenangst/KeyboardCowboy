b2b1e82dbd35e77f78ae15c0918e3ed10b191d9c
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-07-17 16:52:04 +0200

    Refactor ShortcutsCommandRunner
    - Add support for returning the output of a shortcuts command

--- App/Sources/Core/Runners/Shortcuts/ShortcutsCommandRunner.swift
+++ App/Sources/Core/Runners/Shortcuts/ShortcutsCommandRunner.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Intents
 
 final class ShortcutsCommandRunner {
   private let commandRunner: ScriptCommandRunner
@@ -7,13 +8,13 @@ final class ShortcutsCommandRunner {
     self.commandRunner = commandRunner
   }
 
-  func run(_ command: ShortcutCommand) async throws {
+  func run(_ command: ShortcutCommand) async throws -> String? {
     let source = """
     shortcuts run "\(command.shortcutIdentifier)"
     """
     let shellScript = ScriptCommand(
       id: "ShortcutCommand.\(command.shortcutIdentifier)",
       name: command.name, kind: .shellScript, source: .inline(source), notification: false)
-    _ = try await commandRunner.run(shellScript)
+    return try await commandRunner.run(shellScript)
   }
 }
