5acf519f50ff2df0d936d47031a7eaf1e984d042
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-07-18 12:34:06 +0200

    Add documentation to the OpenURLSwapTabsPlugin

--- App/Sources/Core/Runners/Open/Plugins/OpenURLSwapTabsPlugin.swift
+++ App/Sources/Core/Runners/Open/Plugins/OpenURLSwapTabsPlugin.swift
@@ -16,52 +16,77 @@ final class OpenURLSwapTabsPlugin {
     self.commandRunner = commandRunner
   }
 
+  /// This function executes the open command, which opens a URL in a specific application.
+  /// If the target application is already running, it uses accessibility services to find and activate the window/tab with the matching URL. If accessibility services fail, it will run an AppleScript as the fallback to switch to the correct tab.
+  /// If the target application is not running, it opens the URL directly.
+  /// - Parameters:
+  ///   - command: The open command to execute.
+  /// - Throws: An error if the URL cannot be opened.
   func execute(_ command: OpenCommand) async throws {
+    // Get the bundle identifier of the target application, default to Safari if not provided
     let bundleIdentifier = command.application?.bundleIdentifier ?? "com.apple.Safari"
+
+    // Check if the target application is already running
     if let runningApplication = NSWorkspace.shared.runningApplications
       .first(where: { $0.bundleIdentifier == bundleIdentifier }) {
+
       let axApp = AppAccessibilityElement(runningApplication.processIdentifier)
       let windows = try axApp.windows()
+
+      // Flag to track if the URL was successfully opened using accessibility services
       var success: Bool = false
 
       for window in windows {
+        // Find the URL attribute of the web area in the window that matches the command path
         if let url: URL = window.findAttribute(.url, of: "AXWebArea"),
-            url.absoluteString.contains(command.path) {
+           url.absoluteString.contains(command.path) {
           window.performAction(.raise)
-          success = true
           runningApplication.activate(options: .activateIgnoringOtherApps)
+          success = true
           break
         }
       }
 
       if !success {
         let appName = command.application?.displayName ?? "Safari"
+
+        // Create an AppleScript to search for the URL in tabs
+        //
+        // This AppleScript code opens a specific URL in a specific application by swapping tabs.
+        // It takes the path of the URL and the name of the application as input.
+        // It activates the application and checks the URL of every tab in every window.
+        // If it finds a tab with a URL that contains the desired URL, it sets that window as the active window and switches to the corresponding tab.
+        // If it successfully swaps the tab, it returns 0. Otherwise, it returns -1.
         let source = """
-property wantedURL : "\(command.path)"
-tell application "\(appName)"
-  activate
-  set theURLs to (get URL of every tab of every window)
-  repeat with x from 1 to length of theURLs
-    set tmp to item x of theURLs
-    repeat with y from 1 to length of tmp
-      if item y of tmp contains wantedURL then
-        set the index of window x to 1
-        tell window 1
-          if index of current tab is not y then set current tab to tab y
-          return 0
-        end tell
-      end if
-    end repeat
-  end repeat
-  return -1
-end tell
-"""
+            property matchingURL : "\(command.path)"
+            tell application "\(appName)"
+            activate
+            set theURLs to (get URL of every tab of every window)
+            repeat with x from 1 to length of theURLs
+            set tmp to item x of theURLs
+            repeat with y from 1 to length of tmp
+            if item y of tmp contains matchingURL then
+                set the index of window x to 1
+                tell window 1
+                    if index of current tab is not y then set current tab to tab y
+                    return 0
+                end tell
+            end if
+            end repeat
+            end repeat
+            return -1
+            end tell
+            """
+
         let scriptCommand = ScriptCommand(name: UUID().uuidString, kind: .appleScript, source: .inline(source), notification: false)
+
+        // Run the script command and check the result
         if try await commandRunner.run(scriptCommand) == "-1" {
           throw OpenURLSwapToPluginError.couldNotFindOpenUrl
         }
       }
     } else if let url = URL(string: command.path) {
+      // If the target application is not running, open the URL directly
       let configuration = NSWorkspace.OpenConfiguration()
       if let application = command.application {
         try await NSWorkspace.shared.open([url], withApplicationAt: URL(filePath: application.path),
