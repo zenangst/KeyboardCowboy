7513c06b6e2e5b670c4b1bdb36abed8a922628bb
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-07-29 17:56:58 +0200

    Change to .easeInOut animation

--- App/Sources/UI/Reducers/DetailViewActionReducer.swift
+++ App/Sources/UI/Reducers/DetailViewActionReducer.swift
@@ -37,7 +37,7 @@ final class DetailViewActionReducer {
         }
 
         selectionManager.publish(newIds)
-        result = .animated(.interactiveSpring(duration: 0.15, extraBounce: 0.0, blendDuration: 0.25))
+        result = .animated(.easeInOut(duration: 0.15))
       case .togglePassthrough:
         if case .keyboardShortcuts(var previousTrigger) = workflow.trigger {
           previousTrigger.passthrough.toggle()
