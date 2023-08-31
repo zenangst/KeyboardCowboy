4f3e056677c8f6b3570c11ec009bd9f5a8b5eb5a
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-08-31 18:45:52 +0200

    Refactor content images into seperate views
    - Improve performance for shortcut & script by using `IconView` internally, which handles images loading off the main thread.
    - Reduces the clutter inside `ContentImageView`

---
+++ App/Sources/UI/Views/ContentScriptImageView.swift
@@ -0,0 +1,51 @@
+import SwiftUI
+
+struct ContentScriptImageView: View {
+  let source: ScriptCommand.Source
+  let size: CGFloat
+
+  @ViewBuilder
+  var body: some View {
+    switch source {
+    case .inline:
+      ZStack {
+        Rectangle()
+          .fill(LinearGradient(stops: [
+            .init(color: Color.accentColor.opacity(0.2), location: 0.0),
+            .init(color: .black, location: 0.2),
+            .init(color: .black, location: 1.0),
+          ], startPoint: .top, endPoint: .bottom))
+          .cornerRadius(8)
+          .scaleEffect(0.9)
+        RoundedRectangle(cornerRadius: 8)
+          .stroke(.black)
+          .scaleEffect(0.9)
+
+        Text(">_")
+          .font(Font.system(.caption, design: .monospaced))
+      }
+      .frame(width: size, height: size)
+    case .path:
+      IconView(
+        icon: .init(
+          bundleIdentifier: "/System/Applications/Utilities/Script Editor.app",
+          path: "/System/Applications/Utilities/Script Editor.app"
+        ),
+        size: .init(width: size, height: size)
+      )
+        .aspectRatio(1, contentMode: .fill)
+        .frame(width: 32)
+    }
+  }
+}
+
+struct ContentScriptImageView_Previews: PreviewProvider {
+  static var previews: some View {
+    Group {
+      ContentScriptImageView(source: .inline("Hello world!"), size: 32)
+      ContentScriptImageView(source: .path("~/"), size: 32)
+    }
+    .frame(minWidth: 200, minHeight: 120)
+    .padding()
+  }
+}
