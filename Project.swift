3b73ffefd3cf0d349d0a578866db696339d0a287
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-09-23 19:53:31 +0200

    Release 3.13.1

--- Project.swift
+++ Project.swift
@@ -53,10 +53,10 @@ let project = Project(
                         "ASSETCATALOG_COMPILER_APPICON_NAME": "AppIcon",
                         "CODE_SIGN_IDENTITY": "Apple Development",
                         "CODE_SIGN_STYLE": "Automatic",
-                        "CURRENT_PROJECT_VERSION": "227",
+                        "CURRENT_PROJECT_VERSION": "230",
                         "DEVELOPMENT_TEAM": env["TEAM_ID"],
                         "ENABLE_HARDENED_RUNTIME": true,
-                        "MARKETING_VERSION": "3.13.0",
+                        "MARKETING_VERSION": "3.13.1",
                         "PRODUCT_NAME": "Keyboard Cowboy"
                     ],
                     configurations: [
