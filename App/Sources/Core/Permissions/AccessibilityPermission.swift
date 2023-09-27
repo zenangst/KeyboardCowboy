d85f4aa6c1bdf493ed7e619eca768083e606d221
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-09-27 19:45:10 +0200

    Improve permissions flow by using a timer to check permissions

--- App/Sources/Core/Permissions/AccessibilityPermission.swift
+++ App/Sources/Core/Permissions/AccessibilityPermission.swift
@@ -12,12 +12,23 @@ final class AccessibilityPermission: ObservableObject {
 
   static let shared = AccessibilityPermission()
 
+  private var timer: AnyCancellable?
   private var runningApplicationSubscription: AnyCancellable?
   @Published private(set) var viewModel: PermissionsItemStatus = .request
   @Published private(set) var permission: Permission = .notDetermined
 
   init() {
     permission = checkPermission()
+    switch permission {
+    case .authorized:
+      viewModel = .approved
+    case .pending:
+      viewModel = .pending
+    case .notDetermined:
+      viewModel = .unknown
+    case .denied:
+      viewModel = .unknown
+    }
   }
 
   func subscribe(to publisher: Published<RunningApplication?>.Publisher, onAuthorized: @escaping () -> Void) {
@@ -35,20 +46,42 @@ final class AccessibilityPermission: ObservableObject {
     let trusted = kAXTrustedCheckOptionPrompt.takeUnretainedValue()
     let privOptions = [trusted: true] as CFDictionary
     let _ = AXIsProcessTrustedWithOptions(privOptions)
+    let startDate = Date()
+
     permission = .pending
     viewModel = .pending
+    timer = Timer.publish(every: 1, on: .main, in: .default)
+      .autoconnect()
+      .sink { [weak self] time in
+        guard let self else { return }
+        let newState = self.checkPermission()
+        switch newState {
+        case .authorized:
+          permission = .authorized
+          viewModel = .approved
+          NSApplication.shared.setActivationPolicy(.regular)
+          NSApplication.shared.activate(ignoringOtherApps: true)
+          timer = nil
+        default:
+          let timeout = (time.timeIntervalSinceReferenceDate - startDate.timeIntervalSinceReferenceDate)
+          if timeout > 60 {
+            permission = .denied
+            viewModel = .unknown
+            timer = nil
+          }
+        }
+      }
   }
 
+  @discardableResult
   func checkPermission() -> Permission {
     let trusted = kAXTrustedCheckOptionPrompt.takeUnretainedValue()
     let privOptions = [trusted: false] as CFDictionary
     let accessEnabled = AXIsProcessTrustedWithOptions(privOptions)
 
     if accessEnabled {
-      viewModel = .approved
       return .authorized
     } else {
-      viewModel = .request
       return .denied
     }
   }
