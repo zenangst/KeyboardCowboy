dcd2c9c23233db287d6fe6cca5bddd39df34c429
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-10-11 21:08:33 +0200

    Start implementing padding based size constraints to increase window size command

--- App/Sources/Core/Runners/WindowRunner/WindowRunnerIncreaseWindowSize.swift
+++ App/Sources/Core/Runners/WindowRunner/WindowRunnerIncreaseWindowSize.swift
@@ -10,8 +10,26 @@ final class WindowRunnerIncreaseWindowSize {
                            constrainedToScreen: Bool,
                            currentScreen: NSScreen,
                            mainDisplay: NSScreen) -> CGRect {
+    let dockSize = getDockSize(mainDisplay)
+    let dockPosition = getDockPosition(mainDisplay)
+    let padding = CGFloat(padding)
+    let maxWidth = currentScreen.frame.width - padding * 2
+    let maxHeight = currentScreen.frame.height - padding * 2
     let newValue = CGFloat(byValue)
     var newFrame = originFrame
+
+    var dockRightSize: CGFloat = 0
+    var dockBottomSize: CGFloat = 0
+    var dockLeftSize: CGFloat = 0
+
+    switch dockPosition {
+    case .bottom:
+      dockBottomSize = dockSize
+    case .left:
+      dockLeftSize = dockSize
+    case .right:
+      dockRightSize = dockSize
+    }
 
     switch direction {
     case .leading:
@@ -42,8 +60,19 @@ final class WindowRunnerIncreaseWindowSize {
       newFrame.size.height += newValue
     }
 
+    newFrame.size.width = min(newFrame.width, maxWidth)
+    newFrame.size.height = min(newFrame.height, maxHeight)
+
     if constrainedToScreen {
-      newFrame.origin.x = max(currentScreen.frame.origin.x, newFrame.origin.x)
+      let minX = max(currentScreen.frame.origin.x, padding)
+      let maxX = min(newFrame.origin.x, currentScreen.frame.maxX + maxWidth)
+      newFrame.origin.x = max(maxX, minX)
+
+      var maxY = currentScreen.isMainDisplay
+      ? currentScreen.frame.maxY - newFrame.height  - dockBottomSize
+      : mainDisplay.frame.maxY - currentScreen.visibleFrame.origin.y - originFrame.height
+
+      newFrame.origin.y = max(newFrame.origin.y, padding)
     }
 
     return newFrame
