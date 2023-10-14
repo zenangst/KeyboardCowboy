7b5281bd8964811281e8f8829f8b6dcb3fe6d78f
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-10-14 17:51:45 +0200

    Continue on notification feature
    - Remove `NotificationCoordinator`
    - Remove `@_exported` import of `ZenViewKit`
    - Tweak bezel message when running workflow commands
    - Add support for hitting escape to cancel bundled lookup
    - Clean up `BezelNotificationController`
    - Remove unused views
    - Add generic `NotificationView` to handle placement using `Alignment`

--- App/Sources/UI/Controllers/BezelNotificationController.swift
+++ App/Sources/UI/Controllers/BezelNotificationController.swift
@@ -6,39 +6,23 @@ import SwiftUI
 final class BezelNotificationController {
   static let shared = BezelNotificationController()
 
-  private var subscription: AnyCancellable?
-  private var windowController: NSWindowController?
-  private let subject = PassthroughSubject<BezelNotificationViewModel, Never>()
-
-  lazy var coordinator = BezelNotificationCoordinator()
-
-  private init() {
-    self.subscription = subject
-      .debounce(for: .seconds(1), scheduler: DispatchQueue.main)
-      .sink { [weak self] _ in self?.clean() }
-  }
-
-  private func createWindowController() -> NSWindowController {
+  lazy var windowController: NSWindowController = {
     let contentRect = NSScreen.main?.visibleFrame ?? .init(origin: .zero, size: .init(width: 200, height: 200))
-    let content = BezelNotificationView(publisher: coordinator.publisher)
-    let window = BezelNotificationWindow(contentRect: contentRect, content: content)
+    let content = BezelNotificationView(publisher: publisher)
+    let window = NotificationWindow(contentRect: contentRect, content: content)
     window.setFrame(contentRect, display: false)
     let windowController = NSWindowController(window: window)
     return windowController
-  }
+  }()
+  private lazy var publisher = BezelNotificationPublisher(.init(id: UUID().uuidString, text: ""))
+
+  private init() { }
+
 
   func post(_ notification: BezelNotificationViewModel) {
-    if windowController == nil {
-      windowController = createWindowController()
-    }
     withAnimation(.easeOut(duration: 0.175)) {
-      coordinator.publish(notification)
+      publisher.publish(notification)
     }
-    windowController?.showWindow(nil)
-    subject.send(notification)
-  }
-
-  private func clean() {
-    coordinator.publish(.init(id: UUID().uuidString, text: ""))
+    windowController.showWindow(nil)
   }
 }
---
+++ App/Sources/UI/Views/Notifications/WorkflowNotificationMatchesView.swift
@@ -0,0 +1,50 @@
+import SwiftUI
+
+extension AnyTransition {
+  static var moveAndFade: AnyTransition {
+    .asymmetric(
+      insertion:
+          .scale(scale: 0, anchor: .trailing)
+          .combined(with: .opacity)
+      ,
+      removal:
+          .scale.combined(with: .opacity)
+    )
+  }
+}
+
+struct WorkflowNotificationMatchesView: View {
+  static var animation: Animation = .smooth(duration: 0.2)
+  @ObservedObject var publisher: WorkflowNotificationPublisher
+
+  var body: some View {
+    if !publisher.data.matches.isEmpty {
+      ScrollView {
+        LazyVStack(alignment: .trailing) {
+          ForEach(publisher.data.matches, id: \.id) { workflow in
+            HStack {
+              Text(workflow.name)
+                .font(.caption)
+              Spacer()
+              switch workflow.trigger {
+              case .keyboardShortcuts(let trigger):
+                ForEach(trigger.shortcuts) { shortcut in
+                  WorkflowNotificationKeyView(keyShortcut: shortcut, glow: .constant(false))
+                }
+              case .application, .none:
+                EmptyView()
+              }
+            }
+            .frame(alignment: .trailing)
+            .transition(AnyTransition.moveAndFade.animation(Self.animation))
+          }
+        }
+        .padding()
+      }
+      .scrollIndicators(.hidden)
+      .padding(4)
+      .background(Color(nsColor: .windowBackgroundColor).opacity(0.8))
+      .clipShape(RoundedRectangle(cornerRadius: 8))
+    }
+  }
+}
