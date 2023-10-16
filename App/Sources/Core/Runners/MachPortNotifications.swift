c40d30fe0e42da3cb9697f7bd0447d4cfe8b74c7
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-10-16 12:05:49 +0200

    Fix visual bug with the toggle in the `CommandContainerView` Move notifications to `MachPortNotifications` Add `@AppStorage` configs in `NotificationsSettingsView`

---
+++ App/Sources/Core/Runners/MachPortNotifications.swift
@@ -0,0 +1,69 @@
+import Foundation
+import SwiftUI
+
+final class MachPortNotifications {
+  @AppStorage("Notifications.KeyboardCommands") var notificationKeyboardCommands: Bool = false
+  @AppStorage("Notifications.RunningWorkflows") var notificationRunningWorkflows: Bool = false
+  @AppStorage("Notifications.Bundles") var notificationBundles: Bool = false
+
+  let keyboardShortcutsController: KeyboardShortcutsController
+
+  init(keyboardShortcutsController: KeyboardShortcutsController) {
+    self.keyboardShortcutsController = keyboardShortcutsController
+  }
+
+  func notifyRunningWorkflow(_ workflow: Workflow) {
+    guard notificationRunningWorkflows else { return }
+    if case .keyboardShortcuts(let trigger) = workflow.trigger {
+      Task { @MainActor in
+        WorkflowNotificationController.shared.post(.init(id: workflow.id,
+                                                         workflow: workflow,
+                                                         keyboardShortcuts: trigger.shortcuts))
+      }
+    }
+  }
+
+  func notifyKeyboardCommand(_ workflow: Workflow, command: KeyboardCommand) {
+    guard notificationKeyboardCommands else { return }
+    Task { @MainActor in
+      var keyboardShortcuts = [KeyShortcut]()
+      if case .keyboardShortcuts(let trigger) = workflow.trigger {
+        keyboardShortcuts.append(contentsOf: trigger.shortcuts)
+        keyboardShortcuts.append(.init(id: "spacer", key: "="))
+        keyboardShortcuts.append(contentsOf: command.keyboardShortcuts)
+      }
+
+      WorkflowNotificationController.shared.post(.init(id: workflow.id,
+                                                       workflow: nil,
+                                                       keyboardShortcuts: keyboardShortcuts))
+    }
+  }
+
+  func notifyBundles(_ match: PartialMatch) {
+    guard notificationBundles else { return }
+    let splits = match.rawValue.split(separator: "+")
+    let prefix = splits.count - 1
+    if let workflow = match.workflow,
+       case .keyboardShortcuts(let trigger) = workflow.trigger {
+      let shortcuts = Array(trigger.shortcuts.prefix(prefix))
+      let matches = keyboardShortcutsController.allMatchingPrefix(match.rawValue, shortcutIndexPrefix: prefix)
+
+      Task { @MainActor in
+        WorkflowNotificationController.shared.post(.init(id: workflow.id,
+                                                         matches: matches,
+                                                         glow: true,
+                                                         keyboardShortcuts: shortcuts))
+      }
+    }
+  }
+
+  func reset() {
+    Task { @MainActor in
+      WorkflowNotificationController.shared.post(.init(id: UUID().uuidString,
+                                                       matches: [],
+                                                       glow: false,
+                                                       keyboardShortcuts: []))
+    }
+  }
+
+}
