9369e0bcec96d5ddc21b93a7fbbe954b0c44c81c
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-10-16 20:25:19 +0200

    Move WindowManager to its own file

--- App/Sources/UI/Windows/NotificationWindow.swift
+++ App/Sources/UI/Windows/NotificationWindow.swift
@@ -2,31 +2,6 @@ import AppKit
 import Combine
 import SwiftUI
 
-final class WindowManager: ObservableObject {
-  private let passthrough = PassthroughSubject<Void, Never>()
-  private var subscription: AnyCancellable?
-  weak var window: NSWindow?
-
-  init(subscription: AnyCancellable? = nil, window: NSWindow? = nil) {
-    self.subscription = subscription
-    self.window = window
-  }
-
-  func cancelClose() {
-    subscription?.cancel()
-  }
-
-  func close(after stride: DispatchQueue.SchedulerTimeType.Stride, then: @escaping () -> Void = {}) {
-    subscription = passthrough
-      .debounce(for: stride, scheduler: DispatchQueue.main)
-      .sink { [window] in
-        window?.close()
-        then()
-      }
-    passthrough.send()
-  }
-}
-
 final class NotificationWindow<Content>: NSPanel where Content: View {
   private let manager: WindowManager
   override var canBecomeKey: Bool { false }
