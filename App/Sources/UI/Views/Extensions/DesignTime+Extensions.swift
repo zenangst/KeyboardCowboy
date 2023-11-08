03dc37b30c9e75ce5106732e010f5d0107a71707
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-11-08 09:34:48 +0100

    Add view debugger

--- App/Sources/UI/Views/Extensions/DesignTime+Extensions.swift
+++ App/Sources/UI/Views/Extensions/DesignTime+Extensions.swift
@@ -17,3 +17,65 @@ extension View {
       .environmentObject(ShortcutStore(ScriptCommandRunner(workspace: .shared)))
   }
 }
+
+public extension View {
+  func viewDebugger(border: Bool = false) -> some View {
+    self.modifier(ViewDebugger(border: border))
+  }
+}
+
+struct ViewDebug_Preview: PreviewProvider {
+  static var previews: some View {
+    Text("Hello world!")
+      .frame(width: 400, height: 400)
+      .foregroundColor(.blue)
+      .viewDebugger()
+  }
+}
+
+public struct ViewDebugger: ViewModifier {
+  private let border: Bool
+
+  init(border: Bool) {
+    self.border = border
+  }
+
+  public func body(content: Content) -> some View {
+    if isRunningPreview {
+      content
+        .overlay(GeometryReader { proxy in
+          VStack {
+            HStack {
+              Text("\(Int(proxy.size.width))x\(Int(proxy.size.height))")
+                .font(.system(size: 16, weight: .regular, design: .monospaced))
+                .padding(4)
+                .foregroundColor(.black)
+                .background(Color(.systemYellow).cornerRadius(6.0))
+                .compositingGroup()
+                .shadow(radius: 2, y: 1)
+                .scaleEffect(0.6, anchor: .topLeading)
+                .offset(x: 2, y: 2)
+                .fixedSize()
+              Spacer()
+            }
+            Spacer()
+          }
+          .background(
+            ZStack {
+              RoundedRectangle(cornerRadius: 6)
+                .stroke(style: StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round, dash: [10], dashPhase: 10))
+                .foregroundColor(Color(.black).opacity(0.5))
+              RoundedRectangle(cornerRadius: 6)
+                .stroke(style: StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round, dash: [10]))
+                .foregroundColor(Color(.systemYellow))
+            }
+              .compositingGroup()
+              .shadow(radius: 2, y: 1)
+              .opacity(border ? 1 : 0)
+          )
+        })
+    } else {
+      content
+    }
+  }
+}
