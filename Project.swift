e238ac3028dbcf179a3812c72e155c5438413eb6
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-11-09 23:55:55 +0100

    Update Bonzai and MachPort Package Versions
    - Updated the Bonzai package reference to a new commit `c6877f8d05e3cf84883585802c284bc127f9fd87`.
    - Updated the MachPort package reference from version `3.0.1` to `3.0.2`.

--- Project.swift
+++ Project.swift
@@ -163,13 +163,13 @@ public enum PackageResolver {
         .package(url: "https://github.com/krzysztofzablocki/Inject.git", from: "1.1.0"),
         .package(url: "https://github.com/sparkle-project/Sparkle.git", from: "2.4.1"),
         .package(url: "https://github.com/zenangst/AXEssibility.git", from: "0.0.8"),
-        .package(url: "https://github.com/zenangst/Bonzai.git", .revision("3cc38ad3fa42dea6d87f57306c1fac550c539b8f")),
+        .package(url: "https://github.com/zenangst/Bonzai.git", .revision("c6877f8d05e3cf84883585802c284bc127f9fd87")),
         .package(url: "https://github.com/zenangst/Apps.git", from: "1.4.0"),
         .package(url: "https://github.com/zenangst/Dock.git", from: "1.0.1"),
         .package(url: "https://github.com/zenangst/InputSources.git", from: "1.0.1"),
         .package(url: "https://github.com/zenangst/KeyCodes.git", from: "4.0.5"),
         .package(url: "https://github.com/zenangst/LaunchArguments.git", from: "1.0.1"),
-        .package(url: "https://github.com/zenangst/MachPort.git", from: "3.0.1"),
+        .package(url: "https://github.com/zenangst/MachPort.git", from: "3.0.2"),
         .package(url: "https://github.com/zenangst/Windows.git", from: "1.0.0"),
       ]
     }
