5cadcb6bebd958636820a4a0887abb9fd6c1b336
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-11-11 12:50:58 +0100

    Add Additional System Routines for Common Apps
    Enhanced the routine handling by including new applications in the `CustomSystemRoutine` enum, allowing users to set up custom routines for Mail, News, Notes, and Safari. This will facilitate smoother automation and script execution for common tasks associated with these applications.
    
    - Added `mail` case to represent Mail application
    - Added `news` case for News application
    - Included `notes` to handle Notes application
    - Appended `safari` for Safari web browser
    - Adjusted switch case to include the new applications in appropriate routine handling

--- App/Sources/Core/Runners/System/Routines/CustomSystemRoutine.swift
+++ App/Sources/Core/Runners/System/Routines/CustomSystemRoutine.swift
@@ -1,10 +1,15 @@
 enum CustomSystemRoutine: String {
   case finder = "com.apple.finder"
+  case mail = "com.apple.mail"
+  case news = "com.apple.news"
+  case notes = "com.apple.Notes"
+  case safari = "com.apple.Safari"
   case xcode = "com.apple.dt.Xcode"
 
   func routine(_ application: UserSpace.Application) -> SystemRoutine {
     switch self {
-    case .finder, .xcode:
+    case .finder, .mail, .news,
+         .notes, .safari, .xcode:
       return OpenApplicationWithNoWindowsSystemRoutine(application: application)
     }
   }
