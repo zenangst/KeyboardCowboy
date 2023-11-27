1783ef0983df003283c9a7f68b312fa06d75ee5a
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-11-27 22:26:22 +0100

    Refactor AXScrollAreaResolver for improved element processing
    Optimized the element resolution process in AXScrollAreaResolver by extracting and refactoring the logic of checking for focused or selected elements into a separate, recursive function. This enhances maintainability and readability of the code by reducing complexity within the resolveFocusedElement function and centralizing the decision-making process for finding the correct element.

--- App/Sources/Core/Accessibility/AXScrollAreaResolver.swift
+++ App/Sources/Core/Accessibility/AXScrollAreaResolver.swift
@@ -12,21 +12,35 @@ enum AXScrollAreaResolver {
       .map(AnyAccessibilityElement.init)
     var match: AnyAccessibilityElement?
 
-    for child in children {
-      if (try? child.value(.focused, as: Bool.self)) == true {
-        match = child
-        break
-      }
-      if (try? child.value(.selected, as: Bool.self)) == true {
-        match = child
-        break
-      }
-    }
+    match = try? processElement(AnyAccessibilityElement(parent.reference))
 
     guard let match, let frame = match.frame else {
       throw AXScrollAreaResolverError.noResult
     }
 
     return frame
+  }
+
+  private static func processElement(_ element: AnyAccessibilityElement) throws -> AnyAccessibilityElement? {
+    var resolvedElement: AnyAccessibilityElement?
+    let children = try element.value(.children, as: [AXUIElement].self)
+      .map(AnyAccessibilityElement.init)
+
+    if (try? element.value(.focused, as: Bool.self)) == true {
+      return element
+    }
+
+    if (try? element.value(.selected, as: Bool.self)) == true {
+      return element
+    }
+
+    for child in children {
+      if let result = try? processElement(child) {
+        resolvedElement = result
+        break
+      }
+    }
+
+    return resolvedElement
   }
 }
