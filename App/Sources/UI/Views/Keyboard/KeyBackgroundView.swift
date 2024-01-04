e84c9d1b7e6bfc604e9f48150605c3752a20da52
Christoffer Winterkvist <christoffer@winterkvist.com>
2024-01-04 21:03:44 +0100

    Remove blur from key background view

--- App/Sources/UI/Views/Keyboard/KeyBackgroundView.swift
+++ App/Sources/UI/Views/Keyboard/KeyBackgroundView.swift
@@ -18,7 +18,6 @@ struct KeyBackgroundView: View {
           .fill(Color.black.opacity( colorScheme == .light ? 0.33 : 0.9 ))
           .clipShape(RoundedRectangle(cornerRadius: height * 0.1))
           .offset(x: 0, y: height * 0.025)
-          .blur(radius: 1.0)
           .scaleEffect(CGSize(width: 0.95, height: 1.0))
       )
       .background(
@@ -26,7 +25,6 @@ struct KeyBackgroundView: View {
           .fill(Color.black.opacity( colorScheme == .light ? 0.3 : 0.9 ))
           .clipShape(RoundedRectangle(cornerRadius: height * 0.1))
           .offset(x: 0, y: 1)
-          .blur(radius: 1)
           .scaleEffect(CGSize(width: 0.99, height: 1.0))
           .opacity(isPressed ? 0 : 1)
           .animation(.linear(duration: 0.1), value: isPressed)
