fd3cc43c782707401899a8c88d3444abce65f39d
Christoffer Winterkvist <christoffer@winterkvist.com>
2024-02-12 23:28:27 +0100

    Add release notes view

--- App/Sources/UI/Views/Icons/Releases.swift
+++ App/Sources/UI/Views/Icons/Releases.swift
@@ -1,4 +1,126 @@
+import Bonzai
 import SwiftUI
+
+struct Release3_22_0: View {
+  let size: CGFloat = 128
+  let action: () -> Void
+  var body: some View {
+    VStack(spacing: 8) {
+      HStack(spacing: 16) {
+        ZStack {
+          MacroIconView(.record, size: size)
+            .scaleEffect(0.8)
+            .offset(y: -24)
+            .opacity(0.5)
+          MacroIconView(.record, size: size)
+            .scaleEffect(0.9)
+            .offset(y: -12)
+            .shadow(radius: 2)
+            .opacity(0.5)
+          MacroIconView(.record, size: size)
+            .shadow(radius: 2)
+        }
+        VStack(alignment: .leading) {
+          Text("Keyboard Cowboy")
+            .font(Font.system(size: 16, design: .rounded))
+          Text("3.22.0")
+            .foregroundStyle(.white)
+            .font(Font.system(size: 43, design: .rounded))
+            .allowsTightening(true)
+            .fontWeight(.heavy)
+            .shadow(color: Color(.systemCyan), radius: 10)
+        }
+        .shadow(radius: 2)
+        .frame(width: 268, height: size)
+        .fixedSize()
+        .background {
+          Rectangle()
+            .fill(.black)
+            .overlay {
+              AngularGradient(stops: [
+                .init(color: Color.clear, location: 0.0),
+                .init(color: Color.white.opacity(0.2), location: 0.2),
+                .init(color: Color.clear, location: 1.0),
+              ], center: .bottomLeading)
+
+              LinearGradient(stops: [
+                .init(color: Color.white.opacity(0.2), location: 0),
+                .init(color: Color.clear, location: 0.3),
+              ], startPoint: .top, endPoint: .bottom)
+
+              LinearGradient(stops: [
+                .init(color: Color.clear, location: 0.8),
+                .init(color: Color(.windowBackgroundColor).opacity(0.3), location: 1.0),
+              ], startPoint: .top, endPoint: .bottom)
+
+            }
+        }
+        .iconShape(size)
+      }
+      .padding(.top, 32)
+
+      ZenDivider()
+        .frame(maxWidth: 400)
+        .padding(.top, 8)
+
+      VStack(alignment: .leading, spacing: 16) {
+        HStack(spacing: 12) {
+          MacroIconView(.record, size: 32)
+          Text("Macros: Make Every Keystroke Count!")
+            .font(Font.system(.title2, design: .rounded, weight: .bold))
+        }
+        Text("Rev up your routine with Macros — where doing less isn't just more; it's epic. Say hello to keyboard wizardry that conjures up workflows in the blink of an eye.")
+          .fixedSize(horizontal: false, vertical: true)
+        Text("With Macros, you're not just efficient, you're effortlessly epic!")
+        Divider()
+        HStack(spacing: 4) {
+          Text("Special thanks to")
+          AsyncImage.init(url: URL(string: "https://avatars.githubusercontent.com/u/5180591?v=4")) { image in
+            image
+              .resizable()
+              .aspectRatio(contentMode: .fit)
+              .frame(width: 24, height: 24)
+              .mask { Circle() }
+          } placeholder: {
+            Circle()
+              .frame(width: 24, height: 24)
+          }
+
+          Link("@bforpc", destination: URL(string: "https://github.com/bforpc")!)
+          Text("for supporting the project ❤️")
+        }
+
+      }
+      .frame(width: 380)
+      .roundedContainer(margin: 0)
+      .padding(.top, 8)
+      .padding(.horizontal, 16)
+
+      HStack(spacing: 8) {
+        Button(action: action, label: {
+          Text("About Macros")
+        })
+        .buttonStyle(.zen(.init(color: .systemCyan, hoverEffect: .constant(false))))
+
+        Button(action: action, label: {
+          Text("Got it!")
+        })
+        .buttonStyle(.zen(.init(color: .systemGreen, hoverEffect: .constant(false))))
+      }
+      .padding(.top, 8)
+      .padding(.bottom, 32)
+      .frame(width: 410)
+    }
+    .background(Color(.windowBackgroundColor))
+  }
+}
+
+struct Release3_22_0_Previews: PreviewProvider {
+  static var previews: some View {
+    Release3_22_0 {}
+    .previewDisplayName("Release 3.22.0")
+  }
+}
 
 struct Release3_21_0: PreviewProvider {
   static let size: CGFloat = 96
@@ -66,6 +188,7 @@ struct Release3_21_0: PreviewProvider {
     }
     .padding(64)
     .background(Color(.windowBackgroundColor))
+    .previewDisplayName("Release 3.21.0")
   }
 }
 
