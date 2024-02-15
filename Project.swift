ef7a9a6636aa86354d46ba06f0066a8947254e38
Christoffer Winterkvist <christoffer@winterkvist.com>
2024-02-15 21:14:51 +0100

    Update to tuist 4.2.3

--- Project.swift
+++ Project.swift
@@ -1,5 +1,4 @@
 import ProjectDescription
-import ProjectDescriptionHelpers
 import Foundation
 import Env
 
@@ -17,6 +16,71 @@ let envPath = URL(fileURLWithPath: String(#filePath))
   .replacingOccurrences(of: "file://", with: "")
   .appending(".env")
 let env = EnvHelper(envPath)
+// Main application target
+let mainAppTarget = Target.target(
+  name: "Keyboard-Cowboy",
+  destinations: .macOS,
+  product: .app,
+  bundleId: "com.zenangst.Keyboard-Cowboy",
+  deploymentTargets: .macOS("13.0"),
+  infoPlist: .file(path: .relativeToRoot("App/Info.plist")),
+  sources: sources("App"),
+  resources: resources("App"),
+  entitlements: "App/Entitlements/com.zenangst.Keyboard-Cowboy.entitlements",
+  dependencies: [
+    .package(product: "AXEssibility"),
+    .package(product: "Apps"),
+    .package(product: "Bonzai"),
+    .package(product: "Dock"),
+    .package(product: "Inject"),
+    .package(product: "InputSources"),
+    .package(product: "Intercom"),
+    .package(product: "KeyCodes"),
+    .package(product: "LaunchArguments"),
+    .package(product: "MachPort"),
+    .package(product: "Sparkle"),
+    .package(product: "Windows"),
+  ],
+  settings:
+    Settings.settings(
+      base: [
+        "ASSETCATALOG_COMPILER_APPICON_NAME": "AppIcon",
+        "CODE_SIGN_IDENTITY": "Apple Development",
+        "CODE_SIGN_STYLE": "Automatic",
+        "CURRENT_PROJECT_VERSION": "512",
+        "DEVELOPMENT_TEAM": env["TEAM_ID"],
+        "ENABLE_HARDENED_RUNTIME": true,
+        "MARKETING_VERSION": "3.22.0",
+        "PRODUCT_NAME": "Keyboard Cowboy"
+      ],
+      configurations: [
+        .debug(name: "Debug", xcconfig: "\(xcconfig("Debug"))"),
+        .release(name: "Release", xcconfig: "\(xcconfig("Release"))")
+      ],
+      defaultSettings: .recommended)
+)
+// Unit Tests
+let unitTestTarget = Target.target(
+  name: "UnitTests",
+  destinations: .macOS,
+  product: .unitTests,
+  bundleId: bundleId.appending(".unit-tests"),
+  deploymentTargets: .macOS("13.0"),
+  infoPlist: .file(path: .relativeToRoot("UnitTests/Info.plist")),
+  sources: sources("UnitTests"),
+  dependencies: [
+    .target(name: "Keyboard-Cowboy")
+  ],
+  settings:
+    Settings.settings(base: [
+      "BUNDLE_LOADER": "$(TEST_HOST)",
+      "CODE_SIGN_IDENTITY": "Apple Development",
+      "CODE_SIGN_STYLE": "-",
+      "DEVELOPMENT_TEAM": env["TEAM_ID"],
+      "PRODUCT_BUNDLE_IDENTIFIER": "\(bundleId).UnitTests",
+      "TEST_HOST": "$(BUILT_PRODUCTS_DIR)/Keyboard Cowboy.app/Contents/MacOS/Keyboard Cowboy",
+    ])
+)
 
 let project = Project(
   name: "Keyboard Cowboy",
@@ -29,117 +93,59 @@ let project = Project(
     .release(name: "Release", xcconfig: "\(xcconfig("Release"))")
   ], defaultSettings: .recommended),
   targets: [
-    Target(
-      name: "Keyboard-Cowboy",
-      platform: .macOS,
-      product: .app,
-      bundleId: bundleId,
-      deploymentTarget: DeploymentTarget.macOS(targetVersion: "13.0"),
-      infoPlist: .file(path: .relativeToRoot("App/Info.plist")),
-      sources: sources("App"),
-      resources: resources("App"),
-      entitlements: "App/Entitlements/com.zenangst.Keyboard-Cowboy.entitlements",
-      dependencies: [
-        .package(product: "AXEssibility"),
-        .package(product: "Apps"),
-        .package(product: "Bonzai"),
-        .package(product: "Dock"),
-        .package(product: "Inject"),
-        .package(product: "InputSources"),
-        .package(product: "Intercom"),
-        .package(product: "KeyCodes"),
-        .package(product: "LaunchArguments"),
-        .package(product: "MachPort"),
-        .package(product: "Sparkle"),
-        .package(product: "Windows"),
-      ],
-      settings:
-        Settings.settings(
-          base: [
-            "ASSETCATALOG_COMPILER_APPICON_NAME": "AppIcon",
-            "CODE_SIGN_IDENTITY": "Apple Development",
-            "CODE_SIGN_STYLE": "Automatic",
-            "CURRENT_PROJECT_VERSION": "512",
-            "DEVELOPMENT_TEAM": env["TEAM_ID"],
-            "ENABLE_HARDENED_RUNTIME": true,
-            "MARKETING_VERSION": "3.22.0",
-            "PRODUCT_NAME": "Keyboard Cowboy"
-          ],
-          configurations: [
-            .debug(name: "Debug", xcconfig: "\(xcconfig("Debug"))"),
-            .release(name: "Release", xcconfig: "\(xcconfig("Release"))")
-          ],
-          defaultSettings: .recommended)
-    ),
-    Target(
-      name: "UnitTests",
-      platform: .macOS,
-      product: .unitTests,
-      bundleId: bundleId.appending(".unit-tests"),
-      deploymentTarget: DeploymentTarget.macOS(targetVersion: "13.0"),
-      infoPlist: .file(path: .relativeToRoot("UnitTests/Info.plist")),
-      sources: sources("UnitTests"),
-      dependencies: [
-        .target(name: "Keyboard-Cowboy")
-      ],
-      settings:
-        Settings.settings(base: [
-          "BUNDLE_LOADER": "$(TEST_HOST)",
-          "CODE_SIGN_IDENTITY": "Apple Development",
-          "CODE_SIGN_STYLE": "-",
-          "DEVELOPMENT_TEAM": env["TEAM_ID"],
-          "PRODUCT_BUNDLE_IDENTIFIER": "\(bundleId).UnitTests",
-          "TEST_HOST": "$(BUILT_PRODUCTS_DIR)/Keyboard Cowboy.app/Contents/MacOS/Keyboard Cowboy",
-        ])
-    )
+    mainAppTarget,
+    unitTestTarget
   ],
   schemes: [
-    Scheme(
-      name: "Keyboard-Cowboy",
+    Scheme.scheme(
+      name: mainAppTarget.name,
       shared: true,
       hidden: false,
-      buildAction: .buildAction(targets: ["Keyboard-Cowboy"]),
+      buildAction: .buildAction(targets: [.target(mainAppTarget.name)]),
       testAction: .targets(
-        ["UnitTests"],
-        arguments: .init(environmentVariables: [
-          "SOURCE_ROOT": .init(value: "$(SRCROOT)", isEnabled: true),
-        ], launchArguments: [
-          LaunchArgument(name: "-running-unit-tests", isEnabled: true)
-        ]),
-        options: TestActionOptions.options(
+        [.testableTarget(target: .target(unitTestTarget.name))],
+        arguments: .arguments(
+          environmentVariables: [
+            "SOURCE_ROOT": .environmentVariable(value: "$(SRCROOT)", isEnabled: true)
+          ],
+          launchArguments: [
+            .launchArgument(name: "-running-unit-tests", isEnabled: true)
+          ])
+        ,
+        options: .options(
           coverage: true,
-          codeCoverageTargets: [
-            TargetReference(stringLiteral: "Keyboard-Cowboy")
-          ]
+          codeCoverageTargets: [.target(mainAppTarget.name)]
         )
       ),
       runAction: .runAction(
-        executable: "Keyboard-Cowboy",
-        arguments:
-            .init(environmentVariables: [
-              "SOURCE_ROOT": .init(value: "$(SRCROOT)", isEnabled: true),
-              "APP_ENVIRONMENT_OVERRIDE": .init(value: "development", isEnabled: true),
-            ], launchArguments: [
-              LaunchArgument(name: "-benchmark", isEnabled: false),
-              LaunchArgument(name: "-debugEditing", isEnabled: false),
-              LaunchArgument(name: "-injection", isEnabled: false),
-              LaunchArgument(name: "-disableMachPorts", isEnabled: false),
-            ])
+        executable: .target(mainAppTarget.name),
+        arguments: .arguments(
+          environmentVariables: [
+            "SOURCE_ROOT": .environmentVariable(value: "$(SRCROOT)", isEnabled: true),
+            "APP_ENVIRONMENT_OVERRIDE": .environmentVariable(value: "development", isEnabled: true)
+          ],
+          launchArguments: [
+            .launchArgument(name: "-benchmark", isEnabled: false),
+            .launchArgument(name: "-debugEditing", isEnabled: false),
+            .launchArgument(name: "-injection", isEnabled: false),
+            .launchArgument(name: "-disableMachPorts", isEnabled: false),
+          ]
+        )
       )
     )
   ],
   additionalFiles: [
-    FileElement(stringLiteral: ".env"),
-    FileElement(stringLiteral: ".github/workflows"),
-    FileElement(stringLiteral: ".gitignore"),
-    FileElement(stringLiteral: "ci_scripts"),
-    FileElement(stringLiteral: "Fixtures"),
-    FileElement(stringLiteral: "Project.swift"),
-    FileElement(stringLiteral: "README.md"),
-    FileElement(stringLiteral: "Tuist/Dependencies.swift"),
-    FileElement(stringLiteral: "appcast.xml"),
-    FileElement(stringLiteral: "gh-pages"),
-    FileElement(stringLiteral: "_RELEASE_NOTES.md"),
+    .glob(pattern: .path( ".env")),
+    .glob(pattern: .path( ".github/workflows")),
+    .glob(pattern: .path( ".gitignore")),
+    .glob(pattern: .path( "ci_scripts")),
+    .glob(pattern: .path( "Fixtures")),
+    .glob(pattern: .path( "Project.swift")),
+    .glob(pattern: .path( "README.md")),
+    .glob(pattern: .path( "Tuist/Dependencies.swift")),
+    .glob(pattern: .path( "appcast.xml")),
+    .glob(pattern: .path( "gh-pages")),
+    .glob(pattern: .path( "_RELEASE_NOTES.md")),
   ]
 )
 
