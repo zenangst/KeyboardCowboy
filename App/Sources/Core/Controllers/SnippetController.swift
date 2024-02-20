2c673abde9a2d7580a80bad1652a44360fb74e21
Christoffer Winterkvist <christoffer@winterkvist.com>
2024-02-21 00:21:41 +0100

    Refactor SnippetController

--- App/Sources/Core/Controllers/SnippetController.swift
+++ App/Sources/Core/Controllers/SnippetController.swift
@@ -1,4 +1,5 @@
 import Carbon
+import Cocoa
 import Combine
 import Foundation
 import KeyCodes
@@ -7,9 +8,9 @@ import MachPort
 final class SnippetController: @unchecked Sendable, ObservableObject {
   var isEnabled: Bool = true
 
+  @MainActor
   private var currentSnippet: String = ""
   private var machPortEventSubscription: AnyCancellable?
-  private var snippets: Set<String> = []
   private var snippetsStorage = [String: [Workflow]]()
   private var timeout: Timer?
   private var workflowGroupsSubscription: AnyCancellable?
@@ -45,15 +46,19 @@ final class SnippetController: @unchecked Sendable, ObservableObject {
   func subscribe(to publisher: Published<CGEvent?>.Publisher) {
     machPortEventSubscription = publisher
       .compactMap { $0 }
-      .sink { [weak self] machPortEvent in
-      self?.receiveCGEvent(machPortEvent)
+      .sink { [weak self] cgEvent in
+        guard let self else { return }
+        Task { [cgEvent] in
+          await self.receiveCGEvent(cgEvent)
+        }
     }
   }
 
   // MARK: Private methods
 
+  @MainActor
   private func receiveCGEvent(_ event: CGEvent) {
-    guard isEnabled && !snippets.isEmpty, event.type == .keyDown else { return }
+    guard isEnabled && !snippetsStorage.isEmpty, event.type == .keyDown else { return }
     let keyCode = Int(event.getIntegerValueField(.keyboardEventKeycode))
     let forbiddenKeys = [kVK_Escape, kVK_Space, kVK_Delete]
 
@@ -79,25 +84,42 @@ final class SnippetController: @unchecked Sendable, ObservableObject {
       return
     }
 
+    if displayValue == "." {
+      currentSnippet = ""
+      timeout?.invalidate()
+      return
+    }
+
     currentSnippet = currentSnippet + displayValue
 
     timeout?.invalidate()
     timeout = Timer.scheduledTimer(withTimeInterval: 0.75, repeats: false, block: { [weak self] timer in
       guard let self else { return }
-      currentSnippet = ""
+      Task { @MainActor in
+        self.currentSnippet = ""
+      }
       timer.invalidate()
     })
 
-    if snippets.contains(currentSnippet) {
-      guard let workflows = snippetsStorage[currentSnippet],
-            let machPortEvent = MachPortEvent.empty() else { return }
+    guard let runningApplication = NSWorkspace.shared.frontmostApplication,
+          let bundleIdentifier = runningApplication.bundleIdentifier else { return }
 
+    let globalKey: String = "*." + currentSnippet
+    let localKey: String = bundleIdentifier + "." + currentSnippet
+
+    guard let workflows = snippetsStorage[localKey] ?? snippetsStorage[globalKey],
+          let machPortEvent = MachPortEvent.empty() else {
+      return
+    }
+
+    Task { @MainActor in
       // Clean up snippet before running command
       if let key = VirtualSpecialKey.keys[kVK_Delete] {
         for _ in 0..<currentSnippet.count {
-            _ = try? keyboardCommandRunner.run([.init(key: key)], type: .keyDown, with: nil)
-            _ = try? keyboardCommandRunner.run([.init(key: key)], type: .keyUp, with: nil)
+          _ = try? keyboardCommandRunner.run([.init(key: key)], type: .keyDown, with: nil)
+          _ = try? keyboardCommandRunner.run([.init(key: key)], type: .keyUp, with: nil)
         }
+        try await Task.sleep(for: .milliseconds(10))
       }
 
       for workflow in workflows {
@@ -118,30 +140,39 @@ final class SnippetController: @unchecked Sendable, ObservableObject {
 
   private func receiveGroups(_ groups: [WorkflowGroup]) {
     self.snippetsStorage = [:]
-    self.snippets = []
 
-    let workflows = groups.flatMap { $0.workflows }
+    for group in groups {
+      let bundleIdentifiers: [String]
+      if let rule = group.rule {
+        bundleIdentifiers = rule.bundleIdentifiers
+      } else {
+        bundleIdentifiers = ["*"]
+      }
 
-    for workflow in workflows {
-      guard workflow.isEnabled else { continue }
-        if let trigger = workflow.trigger {
+      bundleIdentifiers.forEach { bundleIdentifier in
+        for workflow in group.workflows {
+          guard workflow.isEnabled else { continue }
+          if let trigger = workflow.trigger {
             switch trigger {
             case .snippet(let trigger):
               guard !trigger.text.isEmpty else { return }
 
-              if let existingWorkflows = snippetsStorage[trigger.text] {
-                snippetsStorage[trigger.text] = existingWorkflows + [workflow]
-              } else {
-                snippetsStorage[trigger.text] = [workflow]
-              }
+              let key = bundleIdentifier + "." + trigger.text
 
-              snippets.insert(trigger.text)
+              if let existingWorkflows = snippetsStorage[trigger.text] {
+                snippetsStorage[key] = existingWorkflows + [workflow]
+              } else {
+                snippetsStorage[key] = [workflow]
+              }
             default: break
             }
-        } else {
+          } else {
             runCommands(in: workflow)
+          }
         }
+      }
     }
+
   }
 
   private func runCommands(in workflow: Workflow) {
@@ -169,3 +200,5 @@ final class SnippetController: @unchecked Sendable, ObservableObject {
     }
   }
 }
+
+extension CGEvent: @unchecked Sendable {}
