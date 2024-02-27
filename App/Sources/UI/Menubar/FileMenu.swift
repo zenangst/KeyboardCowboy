7f6b3e35a8bd51224eaa2de7ac848a3960f3f94e
Christoffer Winterkvist <christoffer@winterkvist.com>
2024-02-27 08:04:21 +0100

    Add `New Command` menu item to File Menu

--- App/Sources/UI/Menubar/FileMenu.swift
+++ App/Sources/UI/Menubar/FileMenu.swift
@@ -2,10 +2,13 @@ import SwiftUI
 
 struct FileMenu: View {
   @EnvironmentObject private var store: GroupStore
+  @EnvironmentObject private var detail: ViewModelPublisher<DetailViewState>
+  @EnvironmentObject private var info: ViewModelPublisher<DetailViewModel.Info>
 
   var onNewConfiguration: () -> Void
   var onNewGroup: () -> Void
   var onNewWorkflow: () -> Void
+  var onNewCommand: (DetailViewModel.Info.ID) -> Void
 
   var body: some View {
     Button(action: onNewConfiguration, label: { Text("New Configuration") })
@@ -14,5 +17,17 @@ struct FileMenu: View {
     Button(action: onNewWorkflow, label: { Text("New Workflow") })
       .keyboardShortcut("n", modifiers: [.option, .command])
       .disabled(store.groups.isEmpty)
+    Button(action: {
+      onNewCommand(info.data.id)
+    }, label: { Text("New Command") })
+      .keyboardShortcut("n", modifiers: [.command])
+      .disabled(!newCommandIsEnabled())
+  }
+
+  private func newCommandIsEnabled() -> Bool {
+    switch detail.data {
+    case .single: true
+    default: false
+    }
   }
 }
