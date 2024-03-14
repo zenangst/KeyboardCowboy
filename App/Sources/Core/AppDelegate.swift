d2e801f89b293c8d9400a5a5537e3a8f216c4987
Christoffer Winterkvist <christoffer@winterkvist.com>
2024-03-14 22:47:29 +0100

    Re-open the main window if the application is opened using the Finder

--- App/Sources/Core/AppDelegate.swift
+++ App/Sources/Core/AppDelegate.swift
@@ -5,4 +5,9 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
   func applicationDidFinishLaunching(_ aNotification: Notification) {
     NSApp.appearance = NSAppearance(named: .darkAqua)
   }
+
+  func applicationDidBecomeActive(_ notification: Notification) {
+    NotificationCenter.default.post(.init(name: Notification.Name("OpenMainWindow")))
+    KeyboardCowboy.activate()
+  }
 }
