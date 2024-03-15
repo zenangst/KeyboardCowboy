51c32e18118f791f21d68f98fc55cd3c218df115
Christoffer Winterkvist <christoffer@winterkvist.com>
2024-03-15 08:56:01 +0100

    Don't open the main window at launch

--- App/Sources/Core/AppDelegate.swift
+++ App/Sources/Core/AppDelegate.swift
@@ -2,11 +2,17 @@ import Cocoa
 import SwiftUI
 
 final class AppDelegate: NSObject, NSApplicationDelegate {
+  private var didLaunch: Bool = false
+
   func applicationDidFinishLaunching(_ aNotification: Notification) {
     NSApp.appearance = NSAppearance(named: .darkAqua)
   }
 
   func applicationDidBecomeActive(_ notification: Notification) {
+    guard didLaunch else {
+      didLaunch = true
+      return
+    }
     NotificationCenter.default.post(.init(name: Notification.Name("OpenMainWindow")))
     KeyboardCowboy.activate()
   }
