3ffd92b02bc4ec2f7b476a90b28ac8912b02c8e2
Christoffer Winterkvist <christoffer@winterkvist.com>
2024-05-07 19:51:03 +0200

    Run SystemWindowRelativeFocus using `MainActor` to fix a crash

--- App/Sources/Core/Runners/System/SystemWindowRelativeFocus.swift
+++ App/Sources/Core/Runners/System/SystemWindowRelativeFocus.swift
@@ -9,67 +9,71 @@ enum SystemWindowRelativeFocus {
     case up, down, left, right
   }
 
-  static func run(_ direction: Direction, snapshot: UserSpace.Snapshot) async throws {
-    var windows = await UserSpace.shared.snapshot(resolveUserEnvironment: false).windows.visibleWindowsInStage
-    let frontMostApplication = snapshot.frontMostApplication
-    let frontMostAppElement = AppAccessibilityElement(frontMostApplication.ref.processIdentifier)
-    var activeWindow: WindowModel?
+  static func run(_ direction: Direction, snapshot: UserSpace.Snapshot) throws {
+    Task { @MainActor in
+      var windows = await UserSpace.shared.snapshot(resolveUserEnvironment: false)
+        .windows
+        .visibleWindowsInStage
+      let frontMostApplication = snapshot.frontMostApplication
+      let frontMostAppElement = AppAccessibilityElement(frontMostApplication.ref.processIdentifier)
+      var activeWindow: WindowModel?
 
-    let focusedWindow = try? frontMostAppElement.focusedWindow()
-    for (offset, window) in windows.enumerated() {
-      // If the activate application doesn't have any focused window,
-      // we should activate the first window in the list.
-      // Examples of this include document based applications, such as Xcode & Safari.
-      guard let focusedWindow else {
-        activeWindow = window
-        windows.remove(at: offset)
-        break
+      let focusedWindow = try? frontMostAppElement.focusedWindow()
+      for (offset, window) in windows.enumerated() {
+        // If the activate application doesn't have any focused window,
+        // we should activate the first window in the list.
+        // Examples of this include document based applications, such as Xcode & Safari.
+        guard let focusedWindow else {
+          activeWindow = window
+          windows.remove(at: offset)
+          break
+        }
+
+        if window.id == focusedWindow.id {
+          activeWindow = window
+          windows.remove(at: offset)
+          break
+        }
       }
 
-      if window.id == focusedWindow.id {
-        activeWindow = window
-        windows.remove(at: offset)
-        break
+      guard let activeWindow = activeWindow else { return }
+
+      let matchedWindow: WindowModel? = switch direction {
+      case .up:
+        windows
+          .filter({ $0.rect.origin.y < activeWindow.rect.origin.y })
+          .sorted(by: { $0.rect.origin.y < $1.rect.origin.y })
+          .first
+      case .down:
+        windows
+          .filter({ $0.rect.origin.y > activeWindow.rect.origin.y })
+          .sorted(by: { $0.rect.origin.y > $1.rect.origin.y })
+          .first
+      case .left:
+        windows
+          .filter({ $0.rect.origin.x < activeWindow.rect.origin.x })
+          .sorted(by: { $0.rect.origin.x > $1.rect.origin.x })
+          .first
+      case .right:
+        windows
+          .filter({ $0.rect.origin.x > activeWindow.rect.origin.x })
+          .sorted(by: { $0.rect.origin.x < $1.rect.origin.x })
+          .first
       }
+
+      guard let matchedWindow else { return }
+
+      let processIdentifier = pid_t(matchedWindow.ownerPid.rawValue)
+      guard let runningApplication = NSRunningApplication(processIdentifier: processIdentifier) else { return }
+      let appElement = AppAccessibilityElement(processIdentifier)
+      let match = try appElement.windows().first(where: { $0.id == matchedWindow.id })
+
+      let activationResult = runningApplication.activate()
+      if !activationResult, let bundleURL = runningApplication.bundleURL {
+        NSWorkspace.shared.open(bundleURL)
+      }
+
+      match?.performAction(.raise)
     }
-
-    guard let activeWindow = activeWindow else { return }
-
-    let matchedWindow: WindowModel? = switch direction {
-    case .up:
-      windows
-        .filter({ $0.rect.origin.y < activeWindow.rect.origin.y })
-        .sorted(by: { $0.rect.origin.y < $1.rect.origin.y })
-        .first
-    case .down:
-      windows
-        .filter({ $0.rect.origin.y > activeWindow.rect.origin.y })
-        .sorted(by: { $0.rect.origin.y > $1.rect.origin.y })
-        .first
-    case .left:
-      windows
-        .filter({ $0.rect.origin.x < activeWindow.rect.origin.x })
-        .sorted(by: { $0.rect.origin.x > $1.rect.origin.x })
-        .first
-    case .right:
-      windows
-        .filter({ $0.rect.origin.x > activeWindow.rect.origin.x })
-        .sorted(by: { $0.rect.origin.x < $1.rect.origin.x })
-        .first
-    }
-
-    guard let matchedWindow else { return }
-
-    let processIdentifier = pid_t(matchedWindow.ownerPid.rawValue)
-    guard let runningApplication = NSRunningApplication(processIdentifier: processIdentifier) else { return }
-    let appElement = AppAccessibilityElement(processIdentifier)
-    let match = try appElement.windows().first(where: { $0.id == matchedWindow.id })
-
-    let activationResult = runningApplication.activate()
-    if !activationResult, let bundleURL = runningApplication.bundleURL {
-      NSWorkspace.shared.open(bundleURL)
-    }
-
-    match?.performAction(.raise)
   }
 }
