6f81b831fbae28c1beebdb8ad111094cdfa8d4d3
Christoffer Winterkvist <christoffer@winterkvist.com>
2024-05-14 19:38:48 +0200

    Improve the SystemWindowRelativeFocus algorithm to make it more reliable Exclude windows with zero alpha from windows when moving around relatively

--- App/Sources/Core/Runners/System/SystemWindowRelativeFocus.swift
+++ App/Sources/Core/Runners/System/SystemWindowRelativeFocus.swift
@@ -44,51 +44,16 @@ enum SystemWindowRelativeFocus {
     var matchedWindow: WindowModel?
     switch direction {
     case .up:
-      matchedWindow = windows
-        .filter({ $0.rect.maxY <= activeWindow.rect.maxY })
-        .sorted(by: { $0.rect.maxY > $1.rect.maxY })
-        .first
-      if matchedWindow == nil {
-        matchedWindow = windows
-          .sorted(by: { $0.rect.maxY < $1.rect.maxY })
-          .last
-      }
+      matchedWindow = findAbove(activeWindow, windows: windows)
     case .down:
-      matchedWindow = windows
-        .filter({ $0.rect.maxY >= activeWindow.rect.maxY })
-        .sorted(by: { $0.rect.maxY < $1.rect.maxY })
-        .first
-      if matchedWindow == nil {
-        matchedWindow = windows
-          .sorted(by: { $0.rect.maxY < $1.rect.maxY })
-          .first
-      }
+      matchedWindow = findBelow(activeWindow, windows: windows)
     case .left:
-      matchedWindow = windows
-        .filter({ $0.rect.origin.x < activeWindow.rect.origin.x })
-        .sorted(by: { $0.rect.origin.x > $1.rect.origin.x })
-        .first
-
-      if matchedWindow == nil {
-        matchedWindow = windows
-          .sorted(by: { $0.rect.origin.x < $1.rect.origin.x })
-          .last
-      }
+      matchedWindow = findLeft(activeWindow, windows: windows)
     case .right:
-      matchedWindow = windows
-        .filter({ $0.rect.origin.x > activeWindow.rect.origin.x })
-        .sorted(by: { $0.rect.origin.x < $1.rect.origin.x })
-        .first
-      if matchedWindow == nil {
-        matchedWindow = windows
-          .sorted(by: { $0.rect.origin.x < $1.rect.origin.x })
-          .first
-      }
+      matchedWindow = findRight(activeWindow, windows: windows)
     }
 
-    guard let matchedWindow else {
-      return
-    }
+    guard let matchedWindow else { return }
 
     let processIdentifier = pid_t(matchedWindow.ownerPid.rawValue)
     guard let runningApplication = NSRunningApplication(processIdentifier: processIdentifier) else { return }
@@ -120,9 +85,165 @@ enum SystemWindowRelativeFocus {
         $0.isOnScreen &&
         $0.rect.size.width > minimumSize.width &&
         $0.rect.size.height > minimumSize.height &&
+        $0.alpha == 1 &&
         !excluded.contains($0.ownerName)
       }
 
     return windows
   }
+
+  private static func findLeft(_ activeWindow: WindowModel, windows: [WindowModel]) -> WindowModel? {
+    var matchedWindow: WindowModel?
+    let midX = activeWindow.rect.midX
+    let midY = activeWindow.rect.midY
+    var cursor = CGPoint(x: midX, y: midY)
+
+    // Iterate through the windows array to find the first window that contains midX
+    while cursor.x > 0 {
+      for window in windows {
+        if window.rect.contains(cursor) && window.rect.minX < activeWindow.rect.minX {
+          matchedWindow = window
+          break
+        }
+      }
+      if matchedWindow != nil {
+        break
+      }
+      cursor.x -= 1
+    }
+
+
+    if matchedWindow == nil {
+      // Fallback to the right side of the screen if no windows are found to the left.
+      matchedWindow = windows
+        .filter({ $0.rect.origin.x < activeWindow.rect.origin.x })
+        .sorted(by: { $0.rect.origin.x > $1.rect.origin.x })
+        .first
+    }
+
+    // Wrap around to the right side of the screen if no windows are found to the left.
+    if matchedWindow == nil {
+      matchedWindow = windows
+        .sorted(by: { $0.rect.origin.x < $1.rect.origin.x })
+        .last
+    }
+
+    return matchedWindow
+  }
+
+  private static func findRight(_ activeWindow: WindowModel, windows: [WindowModel]) -> WindowModel? {
+    var matchedWindow: WindowModel?
+    let midX = activeWindow.rect.midX
+    let midY = activeWindow.rect.midY
+    var cursor = CGPoint(x: midX, y: midY)
+    let screenWidth = NSScreen.main?.frame.width ?? CGFloat.greatestFiniteMagnitude
+
+    // Iterate through the windows array to find the first window that contains midX
+    while cursor.x < screenWidth {
+      for window in windows {
+        if window.rect.contains(cursor) && window.rect.minX > activeWindow.rect.minX {
+          matchedWindow = window
+          break
+        }
+      }
+      if matchedWindow != nil {
+        break
+      }
+      cursor.x += 1
+    }
+
+    // Fallback to the right side of the screen if no windows are found to the left.
+    if matchedWindow == nil {
+      matchedWindow = windows
+        .filter({ $0.rect.origin.x > activeWindow.rect.origin.x })
+        .sorted(by: { $0.rect.origin.x < $1.rect.origin.x })
+        .first
+    }
+
+    // Wrap around to the left side of the screen if no windows are found to the right.
+    if matchedWindow == nil {
+      matchedWindow = windows
+        .sorted(by: { $0.rect.origin.x < $1.rect.origin.x })
+        .first
+    }
+
+    return matchedWindow
+  }
+
+  private static func findAbove(_ activeWindow: WindowModel, windows: [WindowModel]) -> WindowModel? {
+    var matchedWindow: WindowModel?
+    let midX = activeWindow.rect.midX
+    let midY = activeWindow.rect.midY
+    var cursor = CGPoint(x: midX, y: midY)
+
+    // Iterate through the windows array to find the first window that contains midY
+    while cursor.y > 0 {
+      for window in windows {
+        if window.rect.contains(cursor) && window.rect.maxY < activeWindow.rect.maxY {
+          matchedWindow = window
+          break
+        }
+      }
+      if matchedWindow != nil {
+        break
+      }
+      cursor.y -= 1
+    }
+
+    // Fallback to the bottom of the screen if no windows are found above.
+    if matchedWindow == nil {
+      matchedWindow = windows
+        .filter({ $0.rect.maxY <= activeWindow.rect.maxY })
+        .sorted(by: { $0.rect.maxY > $1.rect.maxY })
+        .first
+    }
+
+    // Wrap around to the top of the screen if no windows are found above.
+    if matchedWindow == nil {
+      matchedWindow = windows
+        .sorted(by: { $0.rect.maxY < $1.rect.maxY })
+        .last
+    }
+
+    return matchedWindow
+  }
+
+  private static func findBelow(_ activeWindow: WindowModel, windows: [WindowModel]) -> WindowModel? {
+    var matchedWindow: WindowModel?
+    let midX = activeWindow.rect.midX
+    let midY = activeWindow.rect.midY
+    var cursor = CGPoint(x: midX, y: midY)
+    let screenHeight = NSScreen.main?.frame.height ?? CGFloat.greatestFiniteMagnitude
+
+    while cursor.y < screenHeight {
+      for window in windows {
+        if window.rect.contains(cursor) && window.rect.minY > activeWindow.rect.minY {
+          matchedWindow = window
+          break
+        }
+      }
+      if matchedWindow != nil {
+        break
+      }
+      cursor.y += 1
+    }
+
+    // Fallback to the bottom of the screen if no windows are found below.
+    if matchedWindow == nil {
+      matchedWindow = windows
+        .filter({ $0.rect.maxY >= activeWindow.rect.maxY })
+        .sorted(by: { $0.rect.maxY < $1.rect.maxY })
+        .first
+    }
+
+    // Wrap around to the top of the screen if no windows are found below.
+    if matchedWindow == nil {
+      matchedWindow = windows
+        .sorted(by: { $0.rect.maxY < $1.rect.maxY })
+        .first
+    }
+
+    return matchedWindow
+  }
+
 }
--- App/Sources/UI/Releases/Release3_24_0.swift
+++ App/Sources/UI/Releases/Release3_24_0.swift
@@ -1,8 +1,170 @@
-//
-//  Release3_24_0.swift
-//  Keyboard-Cowboy
-//
-//  Created by Christoffer Winterkvist on 5/9/24.
-//
+import Bonzai
+import SwiftUI
 
-import Foundation
+struct Release3_24_0: View {
+  enum ButtonAction {
+    case done
+  }
+
+  let size: CGFloat = 96
+  let action: (ButtonAction) -> Void
+
+  var body: some View {
+    VStack(spacing: 8) {
+      HStack(spacing: 8) {
+        VStack {
+          HStack {
+            RelativeFocusIconView(.right, size: size)
+            CommandLineIconView(size: size)
+          }
+          HStack {
+            ScriptIconView(size: size)
+            KeyboardIconView("M", size: size)
+          }
+        }
+        VStack(spacing: 8) {
+          HStack(spacing: 8) {
+            VStack(alignment: .leading) {
+              Text("Keyboard Cowboy")
+                .font(Font.system(size: 16, design: .rounded))
+              Text("3.24.0")
+                .foregroundStyle(.white)
+                .font(Font.system(size: 43, design: .rounded))
+                .allowsTightening(true)
+                .fontWeight(.heavy)
+                .shadow(color: Color(.systemPink), radius: 10)
+            }
+            .shadow(radius: 2)
+            .frame(width: 304, height: 96)
+            .fixedSize()
+            .background {
+              Rectangle()
+                .fill(.black)
+                .overlay {
+                  AngularGradient(stops: [
+                    .init(color: Color.clear, location: 0.0),
+                    .init(color: Color.white.opacity(0.2), location: 0.2),
+                    .init(color: Color.clear, location: 1.0),
+                  ], center: .bottomLeading)
+
+                  LinearGradient(stops: [
+                    .init(color: Color.white.opacity(0.2), location: 0),
+                    .init(color: Color.clear, location: 0.3),
+                  ], startPoint: .top, endPoint: .bottom)
+
+                  LinearGradient(stops: [
+                    .init(color: Color.clear, location: 0.8),
+                    .init(color: Color(.windowBackgroundColor).opacity(0.3), location: 1.0),
+                  ], startPoint: .top, endPoint: .bottom)
+
+                }
+            }
+            .iconShape(size)
+          }
+
+          HStack {
+            BugFixIconView(size: size)
+            MagicVarsIconView(size: size)
+            EnvironmentIconView(size: size)
+          }
+        }
+      }
+      .padding(.top, 32)
+      .padding(.horizontal, 16)
+
+      ZenDivider()
+        .frame(maxWidth: 400)
+        .padding(.top, 8)
+
+      VStack(alignment: .leading, spacing: 16) {
+        ScrollView(.vertical) {
+          VStack(spacing: 6) {
+            Text("Changes")
+              .font(Font.system(.headline, weight: .bold))
+              .frame(maxWidth: .infinity, alignment: .leading)
+              .padding(.bottom, 6)
+
+            VStack(alignment: .leading, spacing: 12) {
+              HStack(alignment: .top, spacing: 12) {
+                BugFixIconView(size: 24)
+                VStack(spacing: 12) {
+                  Group {
+                    Text("Fixed a bug where application-triggered workflows were inadvertently cancelled during concurrent trigger execution.")
+                  }
+                  .frame(maxWidth: .infinity, alignment: .leading)
+                  Divider()
+                }
+              }
+            }
+
+            VStack(alignment: .leading, spacing: 12) {
+              HStack(alignment: .top, spacing: 12) {
+                KeyboardIconView("M", size: 24)
+                VStack(spacing: 12) {
+                  Text("Resolved an issue causing repeated keyboard events.")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                  Text("Enhanced keyboard command reliability by ensuring commands are consistently sent in pairs.")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                  Divider()
+                }
+              }
+            }
+
+            VStack(alignment: .leading, spacing: 12) {
+              HStack(alignment: .top, spacing: 12) {
+                UIImprovementIconView(size: 24)
+                VStack(spacing: 12) {
+                  Text("Window notifications have been updated to remove the small round indicator when no bundles or notifications are present.")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                  Divider()
+                }
+              }
+            }
+          }
+        }
+        .frame(minHeight: 210)
+
+        Divider()
+
+        HStack(spacing: 4) {
+          Text("Special thanks to")
+          AsyncImage.init(url: URL(string: "https://avatars.githubusercontent.com/u/4262050?v=4")) { image in
+            image
+              .resizable()
+              .aspectRatio(contentMode: .fit)
+              .frame(width: 24, height: 24)
+              .mask { Circle() }
+          } placeholder: {
+            Circle()
+              .fill(Color(.controlAccentColor))
+              .frame(width: 24, height: 24)
+              .overlay {
+                ProgressView()
+              }
+          }
+          Link("@bjrmatos", destination: URL(string: "https://github.com/bjrmatos")!)
+          Text("for supporting the project ❤️")
+        }
+      }
+      .frame(width: 475)
+      .roundedContainer(margin: 0)
+      .padding(.top, 8)
+      .padding(.horizontal, 16)
+
+      HStack(spacing: 8) {
+        Button(action: { action(.done) }, label: { Text("Aww yeah!") })
+          .buttonStyle(.zen(.init(color: .systemGreen, hoverEffect: .constant(false))))
+      }
+      .padding(.top, 8)
+      .padding(.bottom, 32)
+    }
+    .background(Color(.windowBackgroundColor))
+  }
+}
+
+struct Release3_24_0_Previews: PreviewProvider {
+  static var previews: some View {
+    Release3_24_0 { _ in }
+      .previewDisplayName("Release 3.23.2")
+  }
+}
