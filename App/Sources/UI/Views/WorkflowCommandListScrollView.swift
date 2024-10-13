cf01b9e1a45a662227d00346c61550da38a44d17
Christoffer Winterkvist <christoffer@winterkvist.com>
2024-10-13 17:03:51 +0200

    Refactor the command list to use a ZenList

--- App/Sources/UI/Views/WorkflowCommandListScrollView.swift
+++ App/Sources/UI/Views/WorkflowCommandListScrollView.swift
@@ -1,3 +1,4 @@
+import Bonzai
 import SwiftUI
 
 struct WorkflowCommandListScrollView: View {
@@ -32,99 +33,97 @@ struct WorkflowCommandListScrollView: View {
   }
 
   var body: some View {
-    ScrollView {
-      LazyVStack(spacing: 0) {
-        ForEach(publisher.data.commands.lazy, id: \.id) { command in
-          CommandView(
-            focus,
-            command: Binding.readonly(command),
-            publisher: publisher,
-            selectionManager: selectionManager,
-            workflowId: workflowId,
-            onCommandAction: onAction, onAction: { action in
+    ZenList {
+      ForEach(publisher.data.commands, id: \.id) { command in
+        CommandView(
+          focus,
+          command: Binding.readonly(command),
+          publisher: publisher,
+          selectionManager: selectionManager,
+          workflowId: workflowId,
+          onCommandAction: onAction, onAction: { action in
             onAction(.commandView(workflowId: workflowId, action: action))
           })
-          .dropDestination(CommandListDropItem.self,
-                           color: .accentColor,
-                           kind: dropKind,
-                           onDrop: { items, location in
-            var urls = [URL]()
-            for item in items {
-              switch item {
-              case .command:
-                let ids = Array(selectionManager.selections)
-                guard let (from, destination) = publisher.data.commands.moveOffsets(
-                  for: command,
-                  with: ids
-                ) else {
-                  return false
-                }
-
-                withAnimation(WorkflowCommandListView.animation) {
-                  publisher.data.commands.move(fromOffsets: from, toOffset: destination)
-                }
-
-                onAction(.moveCommand(workflowId: workflowId, indexSet: from, toOffset: destination))
-              case .url(let url):
-                urls.append(url)
+        .dropDestination(CommandListDropItem.self,
+                         color: .accentColor,
+                         kind: dropKind,
+                         onDrop: { items, location in
+          var urls = [URL]()
+          for item in items {
+            switch item {
+            case .command:
+              let ids = Array(selectionManager.selections)
+              guard let (from, destination) = publisher.data.commands.moveOffsets(
+                for: command,
+                with: ids
+              ) else {
+                return false
               }
-            }
 
-            if !urls.isEmpty {
-              onAction(.dropUrls(workflowId: workflowId, urls: urls))
+              withAnimation(WorkflowCommandListView.animation) {
+                publisher.data.commands.move(fromOffsets: from, toOffset: destination)
+              }
+
+              onAction(.moveCommand(workflowId: workflowId, indexSet: from, toOffset: destination))
+            case .url(let url):
+              urls.append(url)
             }
-            return true
-          })
-          .contentShape(Rectangle())
-          .contextMenu(menuItems: {
-            WorkflowCommandListContextMenuView(
-              command,
-              workflowId: workflowId,
-              publisher: publisher,
-              selectionManager: selectionManager,
-              onAction: onAction
-            )
-          })
-          .focusable(focus, as: .detail(.command(command.id))) {
-            selectionManager.handleOnTap(publisher.data.commands, element: command)
           }
-        }
-        .padding(.vertical, 5)
-        .onCommand(#selector(NSResponder.insertBacktab(_:)), perform: {
-          switch triggerPublisher.data {
-          case .applications:
-            focus.wrappedValue = .detail(.applicationTriggers)
-          case .keyboardShortcuts:
-            focus.wrappedValue = .detail(.keyboardShortcuts)
-          case .snippet:
-            focus.wrappedValue = .detail(.addSnippetTrigger)
-          case .empty:
-            focus.wrappedValue = .detail(.addAppTrigger)
+
+          if !urls.isEmpty {
+            onAction(.dropUrls(workflowId: workflowId, urls: urls))
           }
+          return true
         })
-        .onCommand(#selector(NSResponder.selectAll(_:)), perform: {
-          selectionManager.publish(Set(publisher.data.commands.map(\.id)))
+        .contentShape(Rectangle())
+        .contextMenu(menuItems: {
+          WorkflowCommandListContextMenuView(
+            command,
+            workflowId: workflowId,
+            publisher: publisher,
+            selectionManager: selectionManager,
+            onAction: onAction
+          )
         })
-        .onMoveCommand(perform: { direction in
-          if let elementID = selectionManager.handle(direction, publisher.data.commands,
-                                                     proxy: scrollViewProxy) {
-            focus.wrappedValue = .detail(.command(elementID))
-          }
-        })
-        .onDeleteCommand {
-          if selectionManager.selections.count == publisher.data.commands.count {
-            withAnimation {
-              onAction(.removeCommands(workflowId: workflowId, commandIds: selectionManager.selections))
-            }
-          } else {
-            onAction(.removeCommands(workflowId: workflowId, commandIds: selectionManager.selections))
-          }
+        .focusable(focus, as: .detail(.command(command.id))) {
+          selectionManager.handleOnTap(publisher.data.commands, element: command)
         }
       }
-      .padding(.horizontal)
-      .padding(.vertical, 8)
-      .focused(focus, equals: .detail(.commands))
-      .matchedGeometryEffect(id: "command-list", in: namespace)
+      .padding(.vertical, 5)
+      .onCommand(#selector(NSResponder.insertBacktab(_:)), perform: {
+        switch triggerPublisher.data {
+        case .applications:
+          focus.wrappedValue = .detail(.applicationTriggers)
+        case .keyboardShortcuts:
+          focus.wrappedValue = .detail(.keyboardShortcuts)
+        case .snippet:
+          focus.wrappedValue = .detail(.addSnippetTrigger)
+        case .empty:
+          focus.wrappedValue = .detail(.addAppTrigger)
+        }
+      })
+      .onCommand(#selector(NSResponder.selectAll(_:)), perform: {
+        selectionManager.publish(Set(publisher.data.commands.map(\.id)))
+      })
+      .onMoveCommand(perform: { direction in
+        if let elementID = selectionManager.handle(direction, publisher.data.commands,
+                                                   proxy: scrollViewProxy) {
+          focus.wrappedValue = .detail(.command(elementID))
+        }
+      })
+      .onDeleteCommand {
+        if selectionManager.selections.count == publisher.data.commands.count {
+          withAnimation {
+            onAction(.removeCommands(workflowId: workflowId, commandIds: selectionManager.selections))
+          }
+        } else {
+          onAction(.removeCommands(workflowId: workflowId, commandIds: selectionManager.selections))
+        }
+      }
     }
+    .padding(.horizontal)
+    .padding(.vertical, 8)
+    .focused(focus, equals: .detail(.commands))
+    .matchedGeometryEffect(id: "command-list", in: namespace)
   }
 }
