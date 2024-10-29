d21dc792f8b5b0b855db8aca38065eb383281933
Christoffer Winterkvist <christoffer@winterkvist.com>
2024-10-29 20:30:51 +0100

    Fix release pipeline by removing the script that didn't run

--- Scripts/release_pipeline/1_build_number.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-CURRENT_VERSION=`agvtool what-version | sed -n 2p | xargs`
-NEW_VERSION=`expr $CURRENT_VERSION + 1`
-
-echo "💪 New build: $NEW_VERSION"
-
-# Bump the current build number
-agvtool new-version $NEW_VERSION
