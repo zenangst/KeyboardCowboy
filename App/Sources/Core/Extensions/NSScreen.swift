1ff2a4eedcd4d8efe5b26f0029fa587166fb59d2
Christoffer Winterkvist <christoffer@winterkvist.com>
2025-01-18 12:29:26 +0100

    Add findNextScreen extension on NSScreen

---
+++ App/Sources/Core/Extensions/NSScreen.swift
@@ -0,0 +1,45 @@
+import Cocoa
+
+extension NSScreen {
+  private func findNextScreen(in direction: WindowTiling, cycling: Bool) -> NSScreen? {
+    let screens = NSScreen.screens
+
+    func screenMatches(_ candidate: NSScreen, direction: WindowTiling, relativeTo currentScreen: NSScreen) -> Bool {
+      switch direction {
+      case .left:
+        return candidate.frame.maxX <= currentScreen.frame.minX
+      case .right:
+        return candidate.frame.minX >= currentScreen.frame.maxX
+      case .top:
+        return candidate.frame.minY >= currentScreen.frame.maxY
+      case .bottom:
+        return candidate.frame.maxY <= currentScreen.frame.minY
+      default:
+        return false
+      }
+    }
+
+    let nextScreen = screens.first(where: { screenMatches($0, direction: direction, relativeTo: self) })
+
+    if let foundScreen = nextScreen {
+      return foundScreen
+    }
+
+    if cycling {
+      switch direction {
+      case .left:
+        return screens.last
+      case .right:
+        return screens.first
+      case .top:
+        return screens.first(where: { $0.frame.maxY > self.frame.maxY }) ?? screens.first
+      case .bottom:
+        return screens.first(where: { $0.frame.minY < self.frame.minY }) ?? screens.first
+      default:
+        return nil
+      }
+    }
+
+    return nil
+  }
+}
