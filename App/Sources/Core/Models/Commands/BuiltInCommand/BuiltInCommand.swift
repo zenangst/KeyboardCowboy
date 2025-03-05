23fd434956d391b92686eadfe27a3b034a683dd0
Christoffer Winterkvist <christoffer@winterkvist.com>
2025-03-05 08:12:16 +0100

    Remove MetaDataMigrator from BuiltInCommand

--- App/Sources/Core/Models/Commands/BuiltInCommand/BuiltInCommand.swift
+++ App/Sources/Core/Models/Commands/BuiltInCommand/BuiltInCommand.swift
@@ -4,12 +4,6 @@ struct BuiltInCommand: MetaDataProviding {
   var meta: Command.MetaData
   let kind: Kind
 
-  enum MigrationCodingKeys: String, CodingKey {
-    case id, kind
-    case isEnabled = "enabled"
-    case notification
-  }
-
   init(id: String = UUID().uuidString, kind: Kind, notification: Command.Notification?) {
     self.kind = kind
     self.meta = .init(id: id, name: kind.displayValue, isEnabled: true, notification: notification)
@@ -22,11 +16,7 @@ struct BuiltInCommand: MetaDataProviding {
 
   init(from decoder: Decoder) throws {
     let container = try decoder.container(keyedBy: CodingKeys.self)
-    do {
-      self.meta = try container.decode(Command.MetaData.self, forKey: .meta)
-    } catch {
-      self.meta = try MetaDataMigrator.migrate(decoder)
-    }
+    self.meta = try container.decode(Command.MetaData.self, forKey: .meta)
     self.kind = try container.decode(BuiltInCommand.Kind.self, forKey: .kind)
   }
 
