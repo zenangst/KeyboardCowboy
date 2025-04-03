1b109c6671a2b6ce0a711ac80d071d7efda4977a
Christoffer Winterkvist <christoffer@winterkvist.com>
2025-04-03 17:33:43 +0200

    Improve hide other apps Improve using hide other apps when using multiple monitors Improve performance of the relative focus command (keep old procedure as a fallback)

--- App/Sources/Core/Runners/System/SystemHideAllAppsRunner.swift
+++ App/Sources/Core/Runners/System/SystemHideAllAppsRunner.swift
@@ -1,3 +1,5 @@
+import Apps
+import AXEssibility
 import Carbon
 import Cocoa
 import MachPort
@@ -6,34 +8,40 @@ import Windows
 final class SystemHideAllAppsRunner {
   @MainActor static var machPort: MachPortEventController?
 
-  static func run(workflowCommands: [Command]) async throws {
-    guard let screen = NSScreen.main else { return }
+  static func run(targetApplication: Application? = nil, workflowCommands: [Command]) async throws {
+    guard let currentScreen = NSScreen.main else { return }
+
+    var targetScreenFrame: CGRect = currentScreen.visibleFrame
 
     let exceptBundleIdentifiers = workflowCommands.compactMap {
       if case .application(let command) = $0, (command.action == .open || command.action == .unhide) { return command.application.bundleIdentifier }
       return nil
     }
 
-    let windows = Set(indexWindowsInStage(getWindows(), targetRect: screen.visibleFrame)
-      .map(\.ownerPid.rawValue))
+    var (processIdentifiers, apps) = Self.runningApplications(in: targetScreenFrame, targetApplication: targetApplication,
+                                                              targetPid: nil, exceptBundleIdentifiers: exceptBundleIdentifiers)
 
-    var apps = NSWorkspace.shared.runningApplications
-      .filter {
-        let processIdentifier = Int($0.processIdentifier)
-        guard windows.contains(processIdentifier) else { return false }
+    if let targetApplication, NSScreen.screens.count > 1,
+       let app = NSWorkspace.shared.runningApplications.first(where: { $0.bundleIdentifier == targetApplication.bundleIdentifier }),
+       !processIdentifiers.contains(Int(app.processIdentifier)) {
 
-        guard !exceptBundleIdentifiers.contains($0.bundleIdentifier ?? "") else { return false }
-        guard let bundleURL = $0.bundleURL else { return false }
+      for screen in NSScreen.screens where screen != currentScreen {
+        let windows = indexWindowsInStage(getWindows(), targetRect: screen.visibleFrame)
+        let pids = Set(windows.map(\.ownerPid.rawValue))
 
-        let pathExtension = (bundleURL.lastPathComponent as NSString).pathExtension
-        let path = bundleURL.path()
-
-        guard pathExtension == "app" else { return false }
-        guard !path.contains("Frameworks/") else { return false }
-
-        return true
+        if pids.contains(Int(app.processIdentifier)) {
+          targetScreenFrame = screen.visibleFrame
+          (processIdentifiers, apps) = Self.runningApplications(in: targetScreenFrame, targetApplication: targetApplication,
+                                                                targetPid: Int(app.processIdentifier),
+                                                                exceptBundleIdentifiers: exceptBundleIdentifiers)
+          break
+        }
       }
-      .filter { $0.activationPolicy == .regular && $0.isHidden == false }
+    }
+
+    guard !apps.isEmpty else {
+      return
+    }
 
     // Fix Podcasts app not hiding when calling `NSRunningApplication.hide()`
     let misbehavingBundles = Set(arrayLiteral: "com.apple.podcasts")
@@ -50,13 +58,14 @@ final class SystemHideAllAppsRunner {
       apps.removeAll(where: { $0.bundleIdentifier == app.bundleIdentifier })
     }
 
-    var processIdentifiers = Set<Int>()
     for app in apps {
       app.hide()
       processIdentifiers.insert(Int(app.processIdentifier))
     }
+
     var timeout: Int = 0
     var waitingForWindowsToDisappear: Bool = true
+    let frontMostPid: Set<Int> = [Int(NSWorkspace.shared.frontmostApplication?.processIdentifier ?? 0)]
 
     while waitingForWindowsToDisappear {
       if timeout >= 10 {
@@ -64,17 +73,45 @@ final class SystemHideAllAppsRunner {
         return
       }
 
-      let windows = indexWindowsInStage(getWindows(), targetRect: screen.visibleFrame)
+      let windows = indexWindowsInStage(getWindows(), targetRect: targetScreenFrame)
       let windowsProcessIds = Set(windows.map(\.ownerPid.rawValue))
 
-      if windowsProcessIds.isDisjoint(with: processIdentifiers) {
+      if windowsProcessIds == frontMostPid, windowsProcessIds.isDisjoint(with: processIdentifiers) {
         waitingForWindowsToDisappear = false
         return
       }
 
       timeout += 1
-      try await Task.sleep(for: .milliseconds(100))
+      try await Task.sleep(for: .milliseconds(10))
     }
+  }
+
+  private static func runningApplications(in targetRect: CGRect, targetApplication: Application? = nil, targetPid: Int?, exceptBundleIdentifiers: [String]) -> (Set<Int>, [NSRunningApplication]) {
+    let processIdentifiers = Set(indexWindowsInStage(getWindows(), targetRect: targetRect)
+      .map(\.ownerPid.rawValue))
+      .filter({ $0 != targetPid })
+
+    let applications = NSWorkspace.shared.runningApplications
+      .filter {
+        guard $0.bundleIdentifier != targetApplication?.bundleIdentifier else { return false }
+
+        let processIdentifier = Int($0.processIdentifier)
+        guard processIdentifiers.contains(processIdentifier) else { return false }
+
+        guard !exceptBundleIdentifiers.contains($0.bundleIdentifier ?? "") else { return false }
+        guard let bundleURL = $0.bundleURL else { return false }
+
+        let pathExtension = (bundleURL.lastPathComponent as NSString).pathExtension
+        let path = bundleURL.path()
+
+        guard pathExtension == "app" else { return false }
+        guard !path.contains("Frameworks/") else { return false }
+
+        return true
+      }
+      .filter { $0.activationPolicy == .regular && $0.isHidden == false }
+
+    return (processIdentifiers, applications)
   }
 
   // MARK: Private methods
