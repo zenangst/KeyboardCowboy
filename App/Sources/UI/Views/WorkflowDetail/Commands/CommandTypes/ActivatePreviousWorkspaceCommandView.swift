65cd884b99915ec278719476a6e7e5757ea6576b
Christoffer Winterkvist <christoffer@winterkvist.com>
2025-05-08 18:11:39 +0200

    Update text in `ActivatePreviousWorkspaceCommandView`

--- App/Sources/UI/Views/WorkflowDetail/Commands/CommandTypes/ActivatePreviousWorkspaceCommandView.swift
+++ App/Sources/UI/Views/WorkflowDetail/Commands/CommandTypes/ActivatePreviousWorkspaceCommandView.swift
@@ -1,7 +1,9 @@
+import Bonzai
 import SwiftUI
 
 struct ActivatePreviousWorkspaceCommandView: View {
   var body: some View {
-    Text("ShowPreviousWorkspaceCommandView")
+    Text("Switch between the current and previous Workspace back and forth.")
+      .style(.derived)
   }
 }
