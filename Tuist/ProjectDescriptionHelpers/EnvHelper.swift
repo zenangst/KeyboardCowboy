3558224a9479eb821f9803e6b1b793b0949c29c1
Christoffer Winterkvist <christoffer@winterkvist.com>
2025-08-06 15:01:36 +0200

    Try a fallback

--- Tuist/ProjectDescriptionHelpers/EnvHelper.swift
+++ Tuist/ProjectDescriptionHelpers/EnvHelper.swift
@@ -8,25 +8,35 @@ public struct EnvHelper: Sendable {
 
   public init(_ path: String) {
     let fileManager = FileManager.default
-    guard fileManager.fileExists(atPath: path) else { fatalError("🌈 \(path) does not exist") }
-    guard let data = fileManager.contents(atPath: path) else { fatalError("🌈 unable to read .env: \(path)") }
-    guard let contents = String(data: data, encoding: .utf8) else { fatalError("🌈 Unable to read data at path: \(path)") }
-
     var env = [String: String]()
-    let lines = contents
-      .components(separatedBy: .newlines)
-      .map { $0.trimmingCharacters(in: .whitespacesAndNewlines) }
-      .filter { !$0.isEmpty }
-      .filter { $0.first != "#" }
-      .filter { $0.contains("=") }
 
-    lines.forEach { line in
-      let components = line.split(separator: "=", maxSplits: 1, omittingEmptySubsequences: true)
-      if components.count == 2 {
-        let name = String(components[0])
-        let value = String(components[1])
-        env[name] = value
+    if fileManager.fileExists(atPath: path) {
+      guard let data = fileManager.contents(atPath: path) else { fatalError("🌈 unable to read .env: \(path)") }
+      guard let contents = String(data: data, encoding: .utf8) else { fatalError("🌈 Unable to read data at path: \(path)") }
+
+      let lines = contents
+        .components(separatedBy: .newlines)
+        .map { $0.trimmingCharacters(in: .whitespacesAndNewlines) }
+        .filter { !$0.isEmpty }
+        .filter { $0.first != "#" }
+        .filter { $0.contains("=") }
+
+      lines.forEach { line in
+        let components = line.split(separator: "=", maxSplits: 1, omittingEmptySubsequences: true)
+        if components.count == 2 {
+          let name = String(components[0])
+          let value = String(components[1])
+          env[name] = value
+        }
       }
+    } else {
+      env = [
+        "APP_NAME" : "Keyboard Cowboy",
+        "APP_SCHEME" : "Keyboard-Cowboy",
+        "APP_BUNDLE_IDENTIFIER" : "com.zenangst.Keyboard-Cowboy",
+        "TEAM_ID" : "XXXXXXXXXX",
+        "PACKAGE_DEVELOPMENT" : "false",
+      ]
     }
 
     self.dictionary = env
